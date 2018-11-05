a007df0df0b1f2b359cc09e5cdeb3da87da63a16
basile <basile.pinsard@gmail.com>
2018-11-05 08:52:49 -0500

    wip

--- src/shared/config.py
+++ src/shared/config.py
@@ -1,5 +1,5 @@
 
-EYETRACKING = False
+EYETRACKING = True
 
 EXP_WINDOW = dict(
     size = (800,600),
@@ -15,7 +15,7 @@ CTL_WINDOW = dict(
 
 EYE_WINDOW = dict(
     size = (640,480),
-    screen=0
+    screen=0,
 )
 
 FRAME_RATE=60
--- src/tasks/memory.py
+++ src/tasks/memory.py
@@ -4,23 +4,30 @@ from .task_base import Task
 
 from ..shared import config
 
-INSTRUCTION_DURATION=4
+INSTRUCTION_DURATION=6
 STIMULI_DURATION=4
 BASELINE_BEGIN=5
 BASELINE_END=5
 ISI=4
 IMAGES_FOLDER = '/home/basile/data/projects/task_stimuli/BOLD5000_Stimuli/Scene_Stimuli/Presented_Stimuli/ImageNet'
 
+quadrant_id_to_pos = [
+    (-.25,.25),
+    (.25,.25),
+    (-.25,-.25),
+    (.25,-.25)
+]
+
 class Images(Task):
 
-    def __init__(self, images_list,*args,**kwargs):
+    def __init__(self, items_list,*args,**kwargs):
         super().__init__(**kwargs)
         #TODO: image lists as params, subjects ....
-        self.image_names = data.importConditions(images_list)
+        self.item_list = data.importConditions(items_list)
 
     def instructions(self, exp_win, ctl_win):
-        instruction_text = """Please keep your eyes open an focused on the screen all the time.
-You will see pictures of scenes and objects."""
+        instruction_text = """You will be presented a set of items in different quadrant of the screen.
+Try to remember the items and their location on the screen."""
         screen_text = visual.TextStim(
             exp_win, text=instruction_text,
             alignHoriz="center", color = 'white')
@@ -32,20 +39,22 @@ You will see pictures of scenes and objects."""
 
     def run(self, exp_win, ctl_win):
 
-        trials = data.TrialHandler(self.image_names, 1, method='sequential')
-        img = visual.ImageStim(exp_win,size=(1,1),units='height')
-        exp_win.logOnFlip(level=logging.EXP,msg='image: task starting at %f'%time.time())
+
+        trials = data.TrialHandler(self.item_list, 1, method='sequential')
+        img = visual.ImageStim(exp_win,size=(.5,.5), units='height')
+        exp_win.logOnFlip(level=logging.EXP,msg='memory: task starting at %f'%time.time())
         for frameN in range(config.FRAME_RATE * BASELINE_BEGIN):
             yield()
         for trial in trials:
-            image_path = os.path.join(IMAGES_FOLDER,trial['image_path'])
+            image_path = os.path.join(IMAGES_FOLDER, trial['image_path'])
             img.image = image_path
-            exp_win.logOnFlip(level=logging.EXP,msg='image: display %s'%image_path)
+            img.pos = quadrant_id_to_pos[trial['quadrant']]
+            exp_win.logOnFlip(level=logging.EXP,msg='memory: display %s in quadrant %d'%(image_path,trial['quadrant']))
             for frameN in range(config.FRAME_RATE * STIMULI_DURATION):
                 img.draw(exp_win)
                 img.draw(ctl_win)
                 yield()
-            exp_win.logOnFlip(level=logging.EXP,msg='image: rest')
+            exp_win.logOnFlip(level=logging.EXP,msg='memory: rest')
             for frameN in range(config.FRAME_RATE * ISI):
                 yield()
         for frameN in range(config.FRAME_RATE * BASELINE_END):
