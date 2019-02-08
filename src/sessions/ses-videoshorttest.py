2251e24ff9b11c1da8ab852cf4144d5ebc5e1332
basile <basile.pinsard@gmail.com>
2019-02-08 10:08:26 -0500

    wip

--- src/sessions/ses-videoshorttest.py
+++ src/sessions/ses-videoshorttest.py
@@ -3,5 +3,5 @@ from ..tasks import  video, task_base
 TASKS = [
 
     video.SingleVideo(
-        'data/videos/test2.mkv', name='testshort'),
+        'data/videos/test2.mp4', name='testshort'),
 ]
