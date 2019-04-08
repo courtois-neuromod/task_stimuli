89f6b6c301526846a9099ffa9df697fd2e22fea5
basile <basile.pinsard@gmail.com>
2019-04-08 16:25:16 -0400

    add first sessions for twows

---
+++ src/sessions/ses-wolfwallstreet1.py
@@ -0,0 +1,15 @@
+from ..tasks import images, video, memory, task_base
+
+TASKS = [
+
+    video.SingleVideo(
+        'data/videos/subject_setup_videos/sub-default_setup_video.mp4', name='Inscapes'),
+
+    task_base.Pause(),
+]
+for seg_idx in range(1,7):
+    TASKS.append(
+        video.SingleVideo(
+            'data/videos/the_wolf_of_wall_street/the_wolf_of_wall_street_seg%02d.mkv'%seg_idx,
+            aspect_ratio = 12/5,
+            name='bourne_supremacy_seg-%d'%seg_idx))
