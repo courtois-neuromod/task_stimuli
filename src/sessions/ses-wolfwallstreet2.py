730a54a865953024633342e2821b60df842e6c1f
courtois-neuromod-machine <courtois-neuromod@gmail.com>
2019-04-11 16:40:13 -0400

    fix names

--- src/sessions/ses-wolfwallstreet2.py
+++ src/sessions/ses-wolfwallstreet2.py
@@ -1,15 +1,9 @@
 from ..tasks import images, video, memory, task_base
 
-TASKS = [
-
-    video.SingleVideo(
-        'data/videos/subject_setup_videos/sub-default_setup_video.mp4', name='Inscapes'),
-
-    task_base.Pause(),
-]
+TASKS = []
 for seg_idx in range(6,13):
     TASKS.append(
         video.SingleVideo(
             'data/videos/the_wolf_of_wall_street/the_wolf_of_wall_street_seg%02d.mkv'%seg_idx,
             aspect_ratio = 12/5,
-            name='bourne_supremacy_seg-%d'%seg_idx))
+            name='the_wolf_of_wall_street_seg-%d'%seg_idx))
