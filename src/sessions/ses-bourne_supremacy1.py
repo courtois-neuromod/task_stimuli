6621abaf3eace051eb152e789eec6de0b663e193
basile <basile.pinsard@gmail.com>
2019-04-11 16:44:03 -0400

    remove inscapes

--- src/sessions/ses-bourne_supremacy1.py
+++ src/sessions/ses-bourne_supremacy1.py
@@ -1,12 +1,6 @@
 from ..tasks import images, video, memory, task_base
 
-TASKS = [
-
-    video.SingleVideo(
-        'data/videos/subject_setup_videos/sub-default_setup_video.mp4', name='Inscapes'),
-
-    task_base.Pause(),
-]
+TASKS = []
 for seg_idx in range(1,6):
     TASKS.append(
         video.SingleVideo(
