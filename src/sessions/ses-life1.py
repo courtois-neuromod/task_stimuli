6ff42edc4d97c244b247706eac00b6abe9a9bee4
basile <basile.pinsard@gmail.com>
2019-04-30 09:41:53 -0400

    fix life1, add delay between tasks to avoid remaining TTL to start next task

--- src/sessions/ses-life1.py
+++ src/sessions/ses-life1.py
@@ -1,7 +1,7 @@
 from ..tasks import images, video, memory, task_base
 
 TASKS = []
-for seg_idx in range(1-6):
+for seg_idx in range(1,6):
     TASKS.append(
         video.SingleVideo(
             'data/videos/life1/life1_seg%02d.mkv'%seg_idx,
