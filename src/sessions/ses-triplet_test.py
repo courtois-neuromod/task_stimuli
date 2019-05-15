5241c2429356656023c0f24f974add27a109ce01
basile <basile.pinsard@gmail.com>
2019-05-15 14:15:27 -0400

    add start key for triplet task for behavioral study, add fixed timing

--- src/sessions/ses-triplet_test.py
+++ src/sessions/ses-triplet_test.py
@@ -3,5 +3,6 @@ from ..tasks import language, task_base
 TASKS = [
     language.Triplet(
         'data/language/triplet/first1000triples.csv',
+        wait_key=True,
         name='triplet_test')
     ]
