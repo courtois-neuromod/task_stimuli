6f8f9f45cf3b074942b0ce7a33c10bdf2b455012
basile <basile.pinsard@gmail.com>
2019-05-29 14:10:28 -0400

    megchanges

--- src/shared/meg.py
+++ src/shared/meg.py
@@ -1,7 +1,9 @@
 from psychopy import parallel
 from . import config
+import time
 
 def send_signal(data):
     port = parallel.ParallelPort(address=config.PARALLEL_PORT_ADDRESS)
     port.setData(data)
+    time.sleep(1)
     port.setData(0) #reset
