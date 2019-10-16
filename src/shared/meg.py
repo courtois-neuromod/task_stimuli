db6d65eb8417384664d9ecb6cf87e80db40d4533
basile <basile.pinsard@gmail.com>
2019-10-16 14:06:58 -0400

    add signal at the beginning and end of task for MEG

--- src/shared/meg.py
+++ src/shared/meg.py
@@ -2,6 +2,11 @@ from psychopy import parallel
 from . import config
 import time
 
+MEG_settings = {
+    'TASK_START_CODE': int("00000010", 2),
+    'TASK_START_STOP': int("00000100", 2),
+}
+
 def send_signal(data):
     port = parallel.ParallelPort(address=config.PARALLEL_PORT_ADDRESS)
     port.setData(data)
