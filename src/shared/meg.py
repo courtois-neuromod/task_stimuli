797c1478a5f6fb578b90af46d55ba468dff0a62f
basile <basile.pinsard@gmail.com>
2019-10-22 13:53:34 -0400

    uploaded changes for the MEG sessions, including triggers, pauses and LPT adress

--- src/shared/meg.py
+++ src/shared/meg.py
@@ -5,5 +5,5 @@ import time
 def send_signal(data):
     port = parallel.ParallelPort(address=config.PARALLEL_PORT_ADDRESS)
     port.setData(data)
-    time.sleep(1)
+    time.sleep(0.001)
     port.setData(0) #reset
