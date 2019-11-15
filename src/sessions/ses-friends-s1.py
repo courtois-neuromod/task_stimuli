083b8e759a1ccb498f329030084bcd2db136ecb0
basile <basile.pinsard@gmail.com>
2019-11-15 17:03:28 -0500

    renaming mkvs

--- src/sessions/ses-friends-s1.py
+++ src/sessions/ses-friends-s1.py
@@ -3,9 +3,9 @@ from ..tasks import video
 TASKS = []
 
 for episode in range(1,25):
-    for segment in range(1,3):
+    for segment in 'ab':
         TASKS.append(
             video.SingleVideo(
-                'data/videos/friends/s1/friends_s1e%02d_seg-%02d.mkv'%(episode, segment),
+                'data/videos/friends/s1/friends_s1e%02d%s.mkv'%(episode, segment),
                 aspect_ratio = 4/3.,
                 name='task-friends-s1e%d-seg%d'%(episode, segment)))
