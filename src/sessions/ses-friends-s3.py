2cd6067ebbb6ffaf7a40462b1f64946dfe911e44
basile <basile.pinsard@gmail.com>
2020-03-09 16:10:28 -0400

    readd files

---
+++ src/sessions/ses-friends-s3.py
@@ -0,0 +1,11 @@
+from ..tasks import video
+
+TASKS = []
+
+for episode in range(1,26):
+    for segment in 'ab':
+        TASKS.append(
+            video.SingleVideo(
+                'data/videos/friends/s3/friends_s3e%02d%s.mkv'%(episode, segment),
+                aspect_ratio = 4/3.,
+                name='task-friends-s3e%d%s'%(episode, segment)))
---
+++ src/sessions/ses-synctest.py
@@ -0,0 +1,7 @@
+from ..tasks import video
+
+TASKS = [
+video.SingleVideo(
+    'data/videos/Sync-Footage-V1-H264.mp4',
+    name='task-synctest')
+] * 10
