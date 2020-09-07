12e66e8836080367e9f4d6924eb96f16c3d6c397
basile <basile.pinsard@gmail.com>
2020-09-07 09:47:44 -0400

    add session script for 3 shinobi levels looping

---
+++ src/sessions/ses-shinobi3levels.py
@@ -0,0 +1,12 @@
+
+from ..tasks import images, videogame, memory, task_base
+
+TASKS = [
+
+    videogame.VideoGameMultiLevel(
+        state_names=['Level1-0','Level4-1','Level5-0'],
+        scenarii=['data/videogames/%s.json'%sc for sc in  ['scenario_repeat1', 'scenario_Level4-1', 'scenario_Level5-1']], # this scenario repeats the same level
+        repeat_scenario=True,
+        max_duration=10*60, # if when level completed or dead we exceed that time in secs, stop the task
+        name='ShinobiIIIReturnOfTheNinjaMaster-multilevel-test')
+] * 5
