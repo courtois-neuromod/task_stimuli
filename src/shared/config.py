39b107c76b14df090f9b3c94674b72649d04d62f
unf <support-unf@criugm.qc.ca>
2020-10-09 15:37:31 -0400

    tests

--- src/shared/config.py
+++ src/shared/config.py
@@ -10,22 +10,24 @@ EYETRACKING_ROI = (60,30,660,450)
 
 EXP_WINDOW = dict(
 #    winType='glfw',
-#    size = (1280,1024),
-    size = (1024, 768),
+    size = (1280,1024),
+#    size = (800, 600),
 #    size = (1920, 1080),
-    screen=1,
+    screen=0,
     fullscr=True,
     gammaErrorPolicy='warn',
+    waitBlanking=False,
 )
 
 CTL_WINDOW = dict(
 #    winType='glfw',
 #    size = (1920, 1080),
     size = (1280, 1024),
+#    size = (1024, 768),
     pos = (100,0),
     screen=0,
     gammaErrorPolicy='warn',
-    swapInterval=0.,
+#    swapInterval=0.,
     waitBlanking=False, # avoid ctrl window to block the script in case of differing refresh rate.
 )
 
