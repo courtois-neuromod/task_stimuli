a9e6e8c57fcd197833ca30f55664104ac578db63
basile <basile.pinsard@gmail.com>
2020-10-27 15:28:46 -0400

    change Things task for non-slip timing,

--- src/shared/cli.py
+++ src/shared/cli.py
@@ -49,12 +49,9 @@ def run_task(task, exp_win, ctl_win=None, eyetracker=None, gaze_drawer=None, rec
         )
 
     if task.use_fmri and not shortcut_evt:
-        shortcut_evt = run_task_loop(
-            fmri.wait_for_ttl(),
-            eyetracker,
-            gaze_drawer
-            )
-        if shortcut_evt: return shortcut_evt
+        while fmri.wait_for_ttl():
+            shortcut_evt = listen_shortcuts()
+            if shortcut_evt: return shortcut_evt
 
     logging.info('GO')
     if eyetracker and not shortcut_evt:
