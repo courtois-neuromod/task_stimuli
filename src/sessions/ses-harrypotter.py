530ba99d4add1ebcaf81bb9ff4fb45f39caa0c19
basile <basile.pinsard@gmail.com>
2021-03-10 11:04:21 -0500

    fixes

--- src/sessions/ses-harrypotter.py
+++ src/sessions/ses-harrypotter.py
@@ -6,6 +6,8 @@ for seg_idx in range(1, 2):
         language.Reading(
             "data/language/triplet/first1000triples.csv",
             name="harrypotter_seg-%d" % seg_idx,
+            cross_duration=2,
+            txt_size=124,
         )
     )
 
