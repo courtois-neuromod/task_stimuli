4867e5be912cefe1a065a5c3fa8b6e0f89bf6281
basile <basile.pinsard@gmail.com>
2021-04-21 14:08:39 -0400

    hog cpu for accurate timing of the MEG marker

--- src/shared/meg.py
+++ src/shared/meg.py
@@ -4,6 +4,8 @@ import time
 
 MEG_MARKERS_ON_FLIP = True
 
+MEG_MARKER_DURATION = .001
+
 MEG_settings = {
     "TASK_START_CODE": int("00000010", 2),
     "TASK_START_STOP": int("00000100", 2),
@@ -16,6 +18,9 @@ def send_signal(data):
     global port
     if not port:
         port = parallel.ParallelPort(address=config.PARALLEL_PORT_ADDRESS)
+    start=time.monotonic()
     port.setData(data)
-    time.sleep(0.001)
+    # hog cpu for accurate timing
+    while time.monotonic() < start + MEG_MARKER_DURATION:
+        continue
     port.setData(0)  # reset
