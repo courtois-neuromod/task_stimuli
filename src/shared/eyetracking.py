2a718691187990125dc3fa6a1b3eb955b16dc448
basile <basile.pinsard@gmail.com>
2021-04-28 14:11:50 -0400

    set 2d detector intensity range to 4 at startup

--- src/shared/eyetracking.py
+++ src/shared/eyetracking.py
@@ -216,6 +216,9 @@ def nonblocking(lock):
 
 
 class EyeTrackerClient(threading.Thread):
+
+    EYE = "eye0"
+
     def __init__(self, output_path, output_fname_base, profile=False, debug=False):
         super(EyeTrackerClient, self).__init__()
         self.stoprequest = threading.Event()
@@ -287,12 +290,26 @@ class EyeTrackerClient(threading.Thread):
             }
         )
 
+        # restart 2d detector plugin with custom output settings
+        self.send_recv_notification(
+            {
+                "subject": "start_eye_plugin",
+                "name": "Detector2DPlugin",
+                "target": self.EYE,
+                "args": {
+                    "properties": {
+                        "intensity_range": 4,
+                    }
+                },
+            }
+        )
+
         # stop a bunch of eye plugins for performance
         for plugin in ["NDSI_Manager"]:
             self.send_recv_notification(
                 {
                     "subject": "stop_eye_plugin",
-                    "target": "eye0",
+                    "target": self.EYE,
                     "name": plugin,
                 }
             )
@@ -301,7 +318,7 @@ class EyeTrackerClient(threading.Thread):
             {
                 "subject": "start_eye_plugin",
                 "name": "Aravis_Source",
-                "target": "eye0",
+                "target": self.EYE,
                 "args": CAPTURE_SETTINGS,
             }
         )
