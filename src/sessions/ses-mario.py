6828cdf25b48fc25ed13e2e9588e5d2d7e3c180b
basile <basile.pinsard@gmail.com>
2021-05-21 16:25:34 -0400

    code adaptive design for mario, required important changes to the code-base, should be tested

--- src/sessions/ses-mario.py
+++ src/sessions/ses-mario.py
@@ -1,12 +1,14 @@
 import os
 import random
 import retro
+import json
 
 # point to a copy of the whole gym-retro with custom states and scenarii
 retro.data.Integrations.add_custom_path(
         os.path.join(os.getcwd(), "data", "videogames", "mario")
 )
 
+from psychopy import logging
 from ..tasks import images, videogame, memory, task_base
 
 flow_ratings = [
@@ -28,7 +30,53 @@ levels_scenario = [
     ("Level1-3", "scenario")]
 #random.shuffle(levels_scenario)  # randomize order
 
+scenario = "scenario"
 
+
+# code adaptive design for learning phase
+
+def get_tasks(parsed):
+    bids_sub = "sub-%s" % parsed.subject
+    savestate_path = os.path.abspath(os.path.join(parsed.output, "sourcedata", bids_sub, f"{bids_sub}_task-mario_savestate.json"))
+
+    # check for a "savestate"
+    if os.path.exists(savestate_path):
+        with open(savestate_path) as f:
+            savestate = json.load(f)
+    else:
+        savestate = {"world": 1, "level":1} #TODO: determine format
+
+    for run in range(5):
+        current_level = f"Level{savestate['world']}-{savestate['level']}"
+        task = videogame.VideoGameMultiLevel(
+            game_name='SuperMarioBros-Nes',
+            state_names=[current_level],
+            scenarii=[scenario],
+            repeat_scenario=True,
+            max_duration=1 * 60,  # if when level completed or dead we exceed that time in secs, stop the task
+            name=f"task-mario_run-{run+1:02d}",
+            instruction="playing Super Mario Bros {state_name} \n\n Let's-a go!",
+            # post_level_ratings = [(q, 7) for q in flow_ratings],
+        )
+        yield task
+
+        if task._completed:
+            logging.exp(f"{current_level} successfuly completed at least once.")
+            savestate['level'] += 1
+            if savestate['level'] > 3:
+                savestate['world'] +=1
+                savestate['level'] = 1
+            with open(savestate_path, 'w') as f:
+                json.dump(savestate, f)
+        else:
+            logging.exp(f"{current_level} not completed.")
+
+        #yield task_base.Pause()
+
+
+    return tasks
+
+"""
 TASKS = sum(
     [
         [
@@ -49,4 +97,4 @@ TASKS = sum(
         for run in range(5)
     ],
     [],
-)
+)"""
