9628e5d6193e565666a1977255fab51479c8c347
Thibault PIRONT <gse.nuks@gmail.com>
2021-06-14 15:33:47 -0400

    fix wind xandr process

--- src/shared/screen.py
+++ src/shared/screen.py
@@ -1,27 +1,30 @@
 from . import config
 from subprocess import Popen
+import os
 
 
 def init_exp_screen():
-    xrandr = Popen(
-        [
-            "xrandr",
-            "--output",
-            config.EXP_SCREEN_XRANDR_NAME,
-            "--mode",
-            "%dx%d" % config.EXP_WINDOW["size"],
-            "--rate",
-            str(config.FRAME_RATE),
-        ]
-    )
+    if os.name != 'nt':
+        xrandr = Popen(
+            [
+                "xrandr",
+                "--output",
+                config.EXP_SCREEN_XRANDR_NAME,
+                "--mode",
+                "%dx%d" % config.EXP_WINDOW["size"],
+                "--rate",
+                str(config.FRAME_RATE),
+            ]
+        )
 
 
 def reset_exp_screen():
-    xrandr = Popen(
-        [
-            "xrandr",
-            "--output",
-            config.EXP_SCREEN_XRANDR_NAME,
-            "--preferred",
-        ]
-    )
+    if os.name != 'nt':
+        xrandr = Popen(
+            [
+                "xrandr",
+                "--output",
+                config.EXP_SCREEN_XRANDR_NAME,
+                "--preferred",
+            ]
+        )
