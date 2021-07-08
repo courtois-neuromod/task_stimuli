7849c1308f60debe2781c41d3290b3a4c722f012
Thibault PIRONT <gse.nuks@gmail.com>
2021-07-07 22:36:44 -0400

    store design generator seed

--- src/sessions/ses-prisme/main.py
+++ src/sessions/ses-prisme/main.py
@@ -1,10 +1,4 @@
 import os
-import re
-import scipy.io
-import numpy as np
-import pandas
-from word_similarity_matrix import generate_similarity_matrix
-from fixed_size_clustering import generate_fixed_size_clusters
 
 # @warning equal value might induce a very slight ns additional delay
 # (due to waiting twice for the same timestamp).
@@ -25,16 +19,36 @@ things_cats_file_path = os.path.join('..', '..', '..', 'data', 'prisme', 'things
 neuromod_image_list_csv_path = os.path.join('data', 'things', 'images', 'image_paths_fmri.csv')
 sensevec_augmented_file_path = os.path.join('data', 'prisme', 'things-metadata', 'Semantic Embedding', 'sensevec_augmented_with_wordvec.mat')
 things_cats_file_path = os.path.join('data', 'prisme', 'things-metadata', 'Variables', 'unique_id.csv')
+fmri_run_count = 8
+eeg_run_count = 3
+display_count = 58
+test_pos_count = 5
+test_neg_count = 5
+
 
 def generate_design_file(subject: str, session: str):
+    import re
+    import scipy.io
+    import numpy as np
+    import pandas
+    import hashlib
+    from word_similarity_matrix import generate_similarity_matrix
+    from fixed_size_clustering import generate_fixed_size_clusters
+
     session_index = int(session)
+
+    # Define random seed for reproducibility (will be stored).
+    seed = int(
+        hashlib.sha1(("%s-%s" % (subject, session)).encode("utf-8")).hexdigest(), 16
+    ) % (2 ** 32 - 1)
+    print('seed', seed)
 
     # Load neuromod/prisme subset of things image dataset
     neuromod_image_list = pandas.read_csv(neuromod_image_list_csv_path)
     neuromod_session_image_list = neuromod_image_list[
         # @note we load all from images_fmri folder, even for test images which
         # we could use from images_test_fmri.
-        neuromod_image_list.condition.eq("exp") &
+        neuromod_image_list.condition.eq('exp') &
         neuromod_image_list.exemplar_nr.eq(session_index)
     ]
 
@@ -72,13 +86,13 @@ def generate_design_file(subject: str, session: str):
 
     # Generate 10 clusters out of the similarity matrix, 5 for shown images to
     # be tested, 5 for unshown images to be tested.
-    cluster_count = 10
+    cluster_count = test_pos_count + test_neg_count
     test_clusters = generate_fixed_size_clusters(neuromod_sensevec_augmented_pd, similarity_matrix, cluster_count)
     print('test_clusters:')
     print(test_clusters['cluster'])
 
     # For each run:
-    run_count = 11 # 8 mri runs, 3 eeg runs
+    run_count = fmri_run_count + eeg_run_count # 8 mri runs, 3 eeg runs
     full_design = pandas.DataFrame()
     for run_idx in range(run_count):
         # Pickup one image out of each test cluster.
@@ -101,7 +115,7 @@ def generate_design_file(subject: str, session: str):
 
         # Generate 53 clusters for the experiment. # 58 shown - 5 shown already
         # within test!
-        cluster_count = 53
+        cluster_count = display_count - test_pos_count
         exp_clusters = generate_fixed_size_clusters(neuromod_sensevec_augmented_pd, similarity_matrix, cluster_count)
         print('exp_clusters:')
         print(exp_clusters['cluster'])
@@ -109,8 +123,8 @@ def generate_design_file(subject: str, session: str):
         # Pickup shown / unshown images to be tested out of it.
         # @warning in case len(test_clusters) > cluster count, some cluster will
         # be ignored.
-        cluster_count = 10
-        shown_idx = np.random.choice(range(cluster_count), size=int(cluster_count/2), replace=False)
+        cluster_count = test_pos_count + test_neg_count
+        shown_idx = np.random.default_rng(seed+(1+run_idx)*1).choice(range(cluster_count), size=int(cluster_count/2), replace=False)
         unshown_idx = np.delete(range(cluster_count), shown_idx)
         shown_images = test_images.iloc[shown_idx]
         unshown_images = test_images.iloc[unshown_idx]
@@ -133,8 +147,8 @@ def generate_design_file(subject: str, session: str):
 
         # Randomize order + ensure we have 53 clusters (output might be a little
         # more, since we aim for perfect fixed-size clusters).
-        cluster_count = 53
-        base_idx = np.random.choice(range(len(exp_images)), size=cluster_count, replace=False)
+        cluster_count = display_count - test_pos_count
+        base_idx = np.random.default_rng(seed+(1+run_idx)*2).choice(range(len(exp_images)), size=cluster_count, replace=False)
         exp_images = exp_images.iloc[base_idx]
         exp_images = exp_images.droplevel('cluster', axis=0)
         print('images:')
@@ -142,8 +156,10 @@ def generate_design_file(subject: str, session: str):
 
         # Inject the 5 test/shown images at +- eq. distance.
         # @todo ensure no out of bound indexes are possible
-        for i in range(5):
-            index = np.random.choice(range(i*int(58/5), (i+1)*int(58/5)), size=1, replace=False)
+        for i in range(test_pos_count):
+            # @warning imperfect insertion boundaries due to float flooring
+            #          even though not dramatic.
+            index = np.random.default_rng(seed+(1+run_idx)*3).choice(range(i*int(display_count/test_pos_count), (i+1)*int(display_count/test_pos_count)), size=1, replace=False)
             index = index[0]
             shown_image = shown_images.iloc[i].to_frame().transpose()  # ensuring it stays a dataframe, not a series.
             exp_images = pandas.concat([exp_images.iloc[:index], shown_image, exp_images.iloc[index:]])
@@ -179,8 +195,8 @@ def generate_design_file(subject: str, session: str):
         test_images['run'] = run_idx + 1
 
         # Setup run type.
-        run_design['type'] = 'fmri' if run_idx <= 8 else 'eeg'
-        test_images['type'] = 'fmri' if run_idx <= 8 else 'eeg'
+        run_design['type'] = 'fmri' if run_idx < fmri_run_count else 'eeg'
+        test_images['type'] = 'fmri' if run_idx < fmri_run_count else 'eeg'
 
         # Concat exp and test images.
         run_design = pandas.concat([run_design, test_images])
@@ -200,13 +216,21 @@ def generate_design_file(subject: str, session: str):
         # Store run within design.
         full_design = pandas.concat([full_design, run_design])
 
-        print("run %d:" % run_idx)
+        print("run %d:" % (run_idx + 1))
         print(run_design)
-    
+
     # Remove label indexes (create duplicates which breaks psychopy
     # importConditions, indexes should be unique).
     full_design['category_name'] = full_design.index
     full_design = full_design.reset_index(drop=True)
+
+    # Store seed.
+    seed_outpath = os.path.join(
+        os.path.join('data', 'prisme', 'designs'),
+        f'sub-{parsed.subject}_ses-{parsed.session}_design-seed.txt',
+    )
+    with open(seed_outpath, 'w') as seed_file:
+        seed_file.write(str(seed))
 
     # Store design within file.
     full_design_outpath = os.path.join(
@@ -214,6 +238,7 @@ def generate_design_file(subject: str, session: str):
         f'sub-{parsed.subject}_ses-{parsed.session}_design.tsv',
     )
     full_design.to_csv(full_design_outpath, sep="\t")
+
 
 # Main function.
 if __name__ == "__main__":
