a4908522861f76fb9a7444e4657db430b25f44de
prisme scan <prisme_scan@eyetracker.unf-montreal.ca>
2021-07-30 10:29:50 -0400

    change fmri ttl input

--- src/shared/fmri.py
+++ src/shared/fmri.py
@@ -4,7 +4,7 @@ import time
 
 MR_settings = {
     "TR": 2.000,  # duration (sec) per whole-brain volume
-    "sync": "5",  # character to use as the sync timing event; assumed to come at start of a volume
+    "sync": "t",  # character to use as the sync timing event; assumed to come at start of a volume
     "skip": 0,  # number of volumes lacking a sync pulse at start of scan (for T1 stabilization)
 }
 
