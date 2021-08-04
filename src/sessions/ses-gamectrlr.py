ee2becb44f59927583bf51a4e89359f53102d7eb
basile <basile.pinsard@gmail.com>
2021-08-04 16:52:51 -0400

    WIP: write game controller button task, add response logging

--- src/sessions/ses-gamectrlr.py
+++ src/sessions/ses-gamectrlr.py
@@ -1,2 +1,102 @@
 import os, sys
 import numpy as np
+
+
+keys = "rludabxy"
+n_sessions = 3
+n_runs = 2
+n_blocks_per_cond = 5
+conditions = ['short', 'long']
+
+final_wait = 9  # time to wait after last trial
+initial_wait = 3  # time until first trial starts
+
+tr = 1.49
+
+short_press_duration = .3
+long_duration_range = [1,3]
+isi_range = [tr, tr*2]
+blocks_isi = 2*tr
+
+def generate_design_file(subject):
+    import pandas
+    import random
+    import hashlib
+
+    # same durations and ISI for all participants/runs
+    np.random.seed(0)
+    durations_set = np.random.rand(len(keys)*n_blocks_per_cond) * \
+        np.diff(long_duration_range) + long_duration_range[0]
+
+    isi_set = np.random.rand(len(keys)*n_blocks_per_cond*len(conditions) - 1)* \
+        np.diff(isi_range) + isi_range[0]
+
+    # seed with subject id to have reproducible design generation
+    seed = int(
+        hashlib.sha1(("%s" % (subject)).encode("utf-8")).hexdigest(), 16
+    ) % (2 ** 32 - 1)
+    print("seed", seed)
+    random.seed(seed)
+    np.random.seed(seed)
+
+    for session in range(n_sessions):
+        for run in range(n_runs):
+            blocks = conditions * n_blocks_per_cond
+            blocks = random.sample(blocks, len(blocks))
+
+            durations = np.random.permutation(durations_set)
+            isis = np.random.permutation(isi_set)
+
+            design = pandas.DataFrame([
+                {'block': block_idx, 'condition': block, 'key': key}
+                for block_idx, block in enumerate(blocks)
+                for key in random.sample(keys, len(keys))
+                ])
+            design['duration'] = short_press_duration
+            design.loc[design.condition=='long','duration'] = durations
+            design.loc[0, 'onset'] = initial_wait
+            design.loc[1:, 'onset'] = (
+                initial_wait +
+                np.cumsum(design['duration'][:-1].to_numpy()) +
+                np.cumsum(isis) +
+                np.cumsum(np.ediff1d(design['block'][:-1].to_numpy(),to_begin=[0]))*blocks_isi)
+
+            out_fname = os.path.join(
+                "data",
+                "game_ctrlr",
+                "designs",
+                f"sub-{parsed.subject}_ses-{session+1:03d}_run-{run+1:02d}_design.tsv",
+            )
+            design.to_csv(out_fname, sep="\t", index=False)
+            print(design.onset[-1:]+design.duration[-1:])
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter,
+        description="generate design files for participant / session",
+    )
+    parser.add_argument("subject", type=str, help="participant id")
+    parsed = parser.parse_args()
+    generate_design_file(parsed.subject)
+
+
+def get_tasks(parsed):
+    from ..tasks.game_controller import ButtonPressTask
+
+    tasks = [
+        ButtonPressTask(
+            os.path.join(
+                "data",
+                "game_ctrlr",
+                "designs",
+                f"sub-{parsed.subject}_ses-{parsed.session}_run-{run:02d}_design.tsv",
+            ),
+            run,
+            name=f"task-gamectrlr_run-{run}")
+        for run in range(1, n_runs+1)
+    ]
+    return tasks
--- src/tasks/game_controller.py
+++ src/tasks/game_controller.py
@@ -1,28 +1,36 @@
 import os, sys, time
 from psychopy import visual, core, data, logging, event
+
 from .task_base import Task
-from .videogames import _onPygletKeyPress, _onPygletKeyRelease
+from .videogame import _onPygletKeyPress, _onPygletKeyRelease, _keyPressBuffer, _keyReleaseBuffer
+from ..shared import config, utils
 
 class ButtonPressTask(Task):
 
     BUTTONS = {
-        'L': [(396,522),(510,522),(568,582),(510,645),(396,645)],
-#        'R': [(396,522),(510,522),(568,582),(510,645),(396,645)],
-
+        'l': [(132,176),(175,176),(196,200),(175,222),(132,222)],
+        'r': [(250,176),(290,176),(290,222),(250,222),(230,200)],
+        'u': [(190,162),(190,120),(232,120),(232,162),(211,184)],
+        'd': [(190,235),(190,276),(232,276),(232,235),(211,215)],
+        'a': [(648, 200), 25],
+        'b': [(592, 253), 25],
+        'x': [(592, 144), 25],
+        'y': [(538, 200), 25],
     }
 
+    FINAL_WAIT = 9
 
-    DEFAULT_INSTRUCTION = """You will be instructed to press the buttons of the controller.
-You will either have to do short presses or hold press until the signal disappears.
-Short presses are instructed by a dot in the center and long-presses by a bar"""
+    DEFAULT_INSTRUCTION = """You will be instructed to press the buttons of the controller for short or long durations."""
 
     def __init__(self, design, run, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        design = data.importConditions(design)
+        self.design = data.importConditions(design)
         self.run_id = run
-        self.design = [trial for trial in design if trial["run"] == run]
+        self.duration = len(self.design)
+        self._progress_bar_refresh_rate = 3 # 3 flips per trial
 
     def _instructions(self, exp_win, ctl_win):
+        return
         screen_text = visual.TextStim(
             exp_win,
             text=self.instruction,
@@ -36,26 +44,75 @@ Short presses are instructed by a dot in the center and long-presses by a bar"""
             if ctl_win:
                 screen_text.draw(ctl_win)
             yield ()
+        if self.run_id == 1:
+            yield True
+            yield from self._long_instructions(exp_win, ctl_win)
+
+    def _long_instructions(self, exp_win, ctl_win):
+        screen_text = visual.TextStim(
+            exp_win,
+            text="""The long bar indicates long keypresses blocks,
+you need to time the press and the release to the button that light-up""",
+            alignText="center",
+            color="white",
+            pos=(0,-.75),
+            wrapWidth=config.WRAP_WIDTH,
+        )
+        self._controller_img.draw(exp_win)
+        screen_text.draw(exp_win)
+        self._cue['long'].draw(exp_win)
+        yield True
+        core.wait(config.INSTRUCTION_DURATION)
+
+        screen_text.text = """The dot indicates short keypresses,
+You have to press and release immediately the button that light-up."""
+        self._controller_img.draw(exp_win)
+        screen_text.draw(exp_win)
+        self._cue['short'].draw(exp_win)
+        yield True
+        core.wait(config.INSTRUCTION_DURATION)
+        yield True
+
 
     def _setup(self, exp_win):
+
+        self.trials = data.TrialHandler(self.design, 1, method="sequential")
 
         self._controller_img = visual.ImageStim(
             exp_win,
             image="data/game_ctrlr/ctrlr.png",
-            size=10,
-            units='deg'
+            size=(800, 481),
+            units="pixels"
         )
 
         self._cue = {
-            'short': visual.Circle(pos=[0,0], radius=5),
-            'long': visual.Line(start=[-20,0], end=[20, 0], lineWidth=5),
+            'short': visual.Circle(
+                exp_win, pos=[0,0], radius=25, units="pixels",
+                lineColor=(255,165,30), fillColor=(255,165,30), colorSpace='rgb255',),
+            'long': visual.Rect(
+                exp_win, width=100., height=20, units="pixels",
+                fillColor=(40,220,120), colorSpace='rgb255',),
             }
 
         self._buttons = {
-            key: (visual.ShapeStim(exp_win, vertices=shape)
+            key: (
+                visual.ShapeStim(
+                    exp_win,
+                    vertices=[(s - self._controller_img.size/2) * (1,-1) for s in shape],
+                    lineWidth=4,
+                    units="pixels",
+                    lineColor=(1,1,1),fillColor=(1,1,1), opacity=.4,)
                 if len(shape)>2
-                else visual.Circle(exp_win, radius=shape[1], pos=shape=[0]))
-                for key, shape in self.BUTTONS.items()}
+                else visual.Circle(
+                    exp_win,
+                    radius=shape[1],
+                    pos=(shape[0] - self._controller_img.size/2) * (1,-1),
+                    lineWidth=4,
+                    units="pixels",
+                    lineColor=(1,1,1),
+                    fillColor=(1,1,1), opacity=.4,))
+                for key, shape in self.BUTTONS.items()
+                }
 
     def _set_key_handler(self, exp_win):
         # activate repeat keys
@@ -89,6 +146,9 @@ Short presses are instructed by a dot in the center and long-presses by a bar"""
 
     def _run(self, exp_win, ctl_win):
 
+        self._set_key_handler(exp_win)
+
+        time_offset = core.getTime() - self.task_timer.getTime()
 
         for trial_n, trial in enumerate(self.trials):
 
@@ -108,7 +168,7 @@ Short presses are instructed by a dot in the center and long-presses by a bar"""
             self._buttons[trial['key']].draw(exp_win)
 
             utils.wait_until(self.task_timer, trial["onset"] - 1 / config.FRAME_RATE)
-            keypresses = event.getKeys(self.RESPONSE_KEYS) # flush response keys
+#            keypresses = event.getKeys(self.RESPONSE_KEYS) # flush response keys
             yield True  # flip
             trial["onset_flip"] = (
                 self._exp_win_last_flip_time - self._exp_win_first_flip_time
@@ -128,8 +188,33 @@ Short presses are instructed by a dot in the center and long-presses by a bar"""
                 self._exp_win_last_flip_time - self._exp_win_first_flip_time
             )
 
-            utils.wait_until(self.task_timer, trial["offset_flip"] + RESPONSE_TIME - 1 / config.FRAME_RATE)
+            utils.wait_until(self.task_timer, trial["offset_flip"] + 1 )
             key_pressed, key_released, other_keys = self._handle_controller_presses(exp_win, [trial['key']])
-            trial['key_press_time'] = key_pressed[0] if key_pressed
-            trial['key_release_time'] = key_released[0] if key_released
-            trial['other_keys'] = other_keys if len(other_keys) # log to exclude trial with confounded keys
+            for k in ['key_press_time', 'key_press_rt', 'key_release_time', 'key_release_rt', 'key_duration']:
+                trial[k] = None
+            if key_pressed is not None:
+                trial['key_press_time'] = key_pressed[1] - time_offset
+                trial['key_press_rt'] = trial['key_press_time']  - trial['onset_flip']
+            if key_released is not None:
+                trial['key_release_time'] = key_released[1] - time_offset
+                if key_pressed is not None:
+                    trial['key_duration'] = trial['key_release_time'] - trial['key_press_time']
+                if trial['condition'] == 'long':
+                    trial['key_release_rt'] = trial['key_release_time']  - trial['offset_flip']
+            trial['other_keys'] = other_keys # log to exclude trial with confounded keys
+            self.progress_bar.set_description(
+                f"Trial {trial_n}:: {trial['condition']} {trial['key']} rt={trial.get('key_press_rt')}"
+            )
+        utils.wait_until(self.task_timer, trial["onset"] + trial['duration'] + FINAL_WAIT)
+        self._unset_key_handler(exp_win)
+
+    def _stop(self, exp_win, ctl_win):
+        self._unset_key_handler(exp_win)
+        yield
+
+    def _restart(self):
+        self.trials = data.TrialHandler(self.design, 1, method="sequential")
+
+    def _save(self):
+        self.trials.saveAsWideText(self._generate_unique_filename("events", "tsv"))
+        return False
