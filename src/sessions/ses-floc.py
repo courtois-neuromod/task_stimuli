3afb397055c2f8486b3aae17ea26fe302166f6aa
MarieStLaurent <marie.stl@gmail.com>
2021-10-28 17:16:18 -0400

    timed to 1.49s TR

--- src/sessions/ses-floc.py
+++ src/sessions/ses-floc.py
@@ -1,5 +1,12 @@
 from ..tasks.localizers import FLoc
 
+#TASKS = [
+#    FLoc(task='OneBack', images_sets='alternate', name='floc'),
+#]
+
+# 'Task': ['OneBack', 'TwoBack', 'Oddball'],
+# 'Image Set': ['default', 'alternate', 'both']
+
 TASKS = [
-    FLoc(task='OneBack', images_sets='alternate', name='floc'),
+    FLoc(task='OneBack', images_sets='default', name='floc', use_eyetracking=True),
 ]
