c6e917f209a39cafd6ae88bdc7d749fd70eb35b8
MarieStLaurent <marie.stl@gmail.com>
2021-10-29 16:09:28 -0400

    two tasks in session and TR synch

--- src/sessions/ses-floc.py
+++ src/sessions/ses-floc.py
@@ -8,5 +8,6 @@ from ..tasks.localizers import FLoc
 # 'Image Set': ['default', 'alternate', 'both']
 
 TASKS = [
-    FLoc(task='OneBack', images_sets='default', name='floc', use_eyetracking=True),
+    FLoc(task='OneBack', images_sets='default', name='task-flocdef', use_eyetracking=True),
+    FLoc(task='OneBack', images_sets='alternate', name='task-flocalt', use_eyetracking=True)    
 ]
