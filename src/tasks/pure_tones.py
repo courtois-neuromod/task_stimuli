dc8f2ba2c52b55f10c7fe3fdce14a60c320460b1
eddyfortier <eddy.fortier@umontreal.ca>
2021-11-03 16:25:28 -0400

    started to refactor code to transform into a class

--- src/tasks/pure_tones.py
+++ src/tasks/pure_tones.py
@@ -1,10 +1,20 @@
 import os
+import argparse
 import time
 import pandas as pd
 import numpy as np
 from playsound import playsound
+# from psychopy.sound.backend_sounddevice import SoundDeviceSound as sds
 
 from ..shared import utils
+
+ls_freq = [250, 500, 1000, 2000,
+           3000, 4000, 6000, 8000]
+ls_intensities = []
+
+sds.secs = 3
+
+
 
 # Time value when the experiment starts
 t_initial = time.perf_counter()
@@ -26,55 +36,61 @@ ls_index = stimuli_df.index.tolist()
 grid = np.full((len(ls_index), len(ls_columns)), True)
 
 
-def fetch_filename():
-    """
-    Returns a wave file name from the list of available files.
-    It also makes sure that the file hasn't already been presented.
-    """
-
-    keep_going = True
-
-    while keep_going:
-        v_value = np.random.randint(0, len(ls_columns))
-        h_value = np.random.randint(0, len(ls_index))
-
-        coordinates = (v_value, h_value)
-
-        v_axis = ls_columns[coordinates[0]]
-        h_axis = ls_index[coordinates[1]]
-        # print(type(grid[coordinates[1], coordinates[0]]))
-
-        if grid[coordinates[1], coordinates[0]] == True:
-            print(True)
-            filename = stimuli_df.at[h_axis, v_axis]
-            grid[coordinates[1], coordinates[0]] = False
-            keep_going = False
-        else:
-            print(False)
-            continue
-
-    return filename
+class PureTones(Task):
 
 
-def stimulus_presentation():
-    """
-    This function fetches a wave file and pleays it.
-    It also provides some feedback to the user regarding:
-    - the number of the stimuli presentation (from 1 to 130)
-    - the inter-stimuli waiting periods.
-    """
+    def fetch_filename():
+        """
+        Returns a wave file name from the list of available files.
+        It also makes sure that the file hasn't already been presented.
+        """
 
-    i = 1
-    while True in grid:
-        to_play = os.path.join(stimuli_path, fetch_filename())
-        print(f"Presentation number {i}: {to_play}")
-        playsound(to_play)
-        i = i + 1
-        print(f"Waiting for {ISI} second(s)")
-        time.sleep(ISI)
+        keep_going = True
+
+        while keep_going:
+            v_value = np.random.randint(0, len(ls_columns))
+            h_value = np.random.randint(0, len(ls_index))
+
+            coordinates = (v_value, h_value)
+
+            v_axis = ls_columns[coordinates[0]]
+            h_axis = ls_index[coordinates[1]]
+            # print(type(grid[coordinates[1], coordinates[0]]))
+
+            if grid[coordinates[1], coordinates[0]] == True:
+                print(True)
+                filename = stimuli_df.at[h_axis, v_axis]
+                grid[coordinates[1], coordinates[0]] = False
+                keep_going = False
+            else:
+                print(False)
+                continue
+
+        return filename
 
 
-stimulus_presentation()
+    def _run():
+        """
+        This function fetches a wave file and pleays it.
+        It also provides some feedback to the user regarding:
+        - the number of the stimuli presentation (from 1 to 130)
+        - the inter-stimuli waiting periods.
+        """
+
+        sub_parser = argparse.ArgumentParser(description="Subject ID")
+
+        i = 1
+        while True in grid:
+            to_play = os.path.join(stimuli_path, fetch_filename())
+            print(f"Presentation number {i}: {to_play}")
+            playsound(to_play)
+            i = i + 1
+            print(f"Waiting for {ISI} second(s)")
+            time.sleep(ISI)
+            # utils.wait_until("self.task_timer", "ISI")
+
+
+_run()
 
 
 # Calculation of the duration of the experiment
