778e9aeea29d16b9f6aa45300e69b4e3eba5c31e
efortier <eddy.fortier@umontreal.ca>
2021-12-14 15:30:56 -0500

    started to adapt __init__ to the design file use (pure_tones.py)

--- src/tasks/pure_tones.py
+++ src/tasks/pure_tones.py
@@ -6,77 +6,76 @@ import numpy as np
 from playsound import playsound
 from psychopy.sound.backend_sounddevice import SoundDeviceSound as sds
 
-#from ..shared import utils
+from .task_base import Task
+# from ..shared import utils
 
 # Time value when the experiment starts
 # t_initial = time.perf_counter()
 
 
-class PureTones():
+class PureTones(Task):
 
-    def _run():
+    def __init__(self, sub, ISI=0, design):
+        super(Task, self).__init__()
+        self.ISI = ISI
+        self.sub = sub
+        self.session_filename = design
+        np.random.RandomState(seed=1)
+
+        # sub_parser = argparse.ArgumentParser(description="subject")
+        # sub_parser = "sub01"  # parsed.get("subject")
+        self.sub_number = ""
+        for i in range(0, len(self.sub)):
+            if self.sub[i].isdigit():
+                self.sub_number += str(self.sub[i])
+            else:
+                continue
+
+        # Stimuli duration (in sec.)
+        # sds.secs = 3
+
+        self.stimuli_path = os.path.join("..", "..", "data", "audio")
+
+#        stimuli_L = pd.read_csv(os.path.join(stimuli_path,
+#                                             ("sub-" + self.sub_number +
+#                                              "_desc-L.tsv")),
+#                                sep="\t")
+#        stimuli_R = pd.read_csv(os.path.join(stimuli_path,
+#                                             ("sub-" + self.sub_number +
+#                                              "_desc-R.tsv")),
+#                                sep="\t")
+
+#        stimuli_df = pd.concat([stimuli_L, stimuli_R], ignore_index=True)
+
+        self.stimuli_df = pd.read_csv(self.session_filename, sep="\t")
+        self.stimuli_ls = self.stimuli_df["trial_type"]
+        print(self.stimuli_ls)
+
+
+    def _run(self, run_number):
         """
-        This function fetches a wave file and pleays it.
+        This function fetches a wave file and plays it.
         It also provides some feedback to the user regarding:
         - the number of the stimuli presentation (from 1 to 130)
         - the inter-stimuli waiting periods.
         """
 
-        np.random.RandomState(seed=1)
-
-        # Waiting period between stimuli presentation (in sec.)
-        ISI = 0
-
-        # sub_parser = argparse.ArgumentParser(description="subject")
-        sub_parser = "sub01" #parsed.get("subject")
-        sub_number = ""
-        for i in range(0, len(sub_parser)):
-            if sub_parser[i].isdigit():
-                sub_number += str(sub_parser[i])
-            else:
-                continue
-
-        # Stimuli duration (in sec.)
-        sds.secs = 3
-
-        stimuli_path = os.path.join("..", "..", "data", "audio")
-
-        stimuli_L = pd.read_csv(os.path.join(stimuli_path,
-                                             "sub-" + sub_number + "_desc-L.tsv"),
-                                sep="\t")
-        stimuli_R = pd.read_csv(os.path.join(stimuli_path,
-                                             "sub-" + sub_number + "_desc-R.tsv"),
-                                sep="\t")
-
-        stimuli_df = pd.concat([stimuli_L, stimuli_R], ignore_index=True)
-
-        stimuli_ls = stimuli_df.to_numpy().flatten().tolist()
+        random_stimuli_ls = np.random.shuffle(self.stimuli_ls)
         
-        # Number of intensities/frequency/ear
-        intensity_count = len(stimuli_df) / 2
-
-        i = 1
-        while len(stimuli_ls) > 0:
-
-            position = np.random.randint(0, len(stimuli_ls))
-            filename = stimuli_ls[position]
+        for i, sound_file in enumerate(random_stimuli_ls):
             
-            to_play = os.path.join(stimuli_path, filename)
-                                   
-            print(f"Presentation number {i}: {to_play}")
+            to_play = os.path.join(self.stimuli_path, sound_file)
+            
+            print(f"Presentation number {i}: {sound_file}")
+            
+            # !!! Needs to be replaced by a psychopy functionality
             playsound(to_play)
             
-            print("avant", len(stimuli_ls), stimuli_ls)
-            del stimuli_ls[position]
-            print("après", len(stimuli_ls), stimuli_ls)
+            print(f"Waiting for {self.ISI} second(s)")
             
-            i = i + 1
-            print(f"Waiting for {ISI} second(s)")
-            time.sleep(ISI)
+            # !!! Needs to be replaced by the utils wait function
+            time.sleep(self.ISI)
             # utils.wait_until("self.task_timer", "ISI")
-
-
-PureTones._run()
 
 
 # Calculation of the duration of the experiment
