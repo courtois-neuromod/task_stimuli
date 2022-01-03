9f17b4c59447ab480129ae67521d5bcb62cceb4f
efortier <eddy.fortier@umontreal.ca>
2022-01-03 17:15:17 -0500

    removed obsolete lines, added a psychopy.prefs correction (pure_tones.py)

--- src/tasks/pure_tones.py
+++ src/tasks/pure_tones.py
@@ -3,8 +3,13 @@ import argparse
 import time
 import pandas as pd
 import numpy as np
+import psychtoolbox as ptb
+
+from psychopy import prefs
+prefs.hardware['audioLib'] = ['PTB']
+
+from psychopy import sound
 from playsound import playsound
-from psychopy.sound.backend_sounddevice import SoundDeviceSound as sds
 
 from .task_base import Task
 # from ..shared import utils
@@ -15,15 +20,13 @@ from .task_base import Task
 
 class PureTones(Task):
 
-    def __init__(self, sub, ISI=0, design):
+    def __init__(self, sub, design, ISI=0):
         super(Task, self).__init__()
         self.ISI = ISI
         self.sub = sub
         self.session_filename = design
         np.random.RandomState(seed=1)
 
-        # sub_parser = argparse.ArgumentParser(description="subject")
-        # sub_parser = "sub01"  # parsed.get("subject")
         self.sub_number = ""
         for i in range(0, len(self.sub)):
             if self.sub[i].isdigit():
@@ -31,42 +34,30 @@ class PureTones(Task):
             else:
                 continue
 
-        # Stimuli duration (in sec.)
-        # sds.secs = 3
-
-        self.stimuli_path = os.path.join("..", "..", "data", "audio")
-
-#        stimuli_L = pd.read_csv(os.path.join(stimuli_path,
-#                                             ("sub-" + self.sub_number +
-#                                              "_desc-L.tsv")),
-#                                sep="\t")
-#        stimuli_R = pd.read_csv(os.path.join(stimuli_path,
-#                                             ("sub-" + self.sub_number +
-#                                              "_desc-R.tsv")),
-#                                sep="\t")
-
-#        stimuli_df = pd.concat([stimuli_L, stimuli_R], ignore_index=True)
+        self.stimuli_path = os.path.join("data", "audio", "stimuli")
 
         self.stimuli_df = pd.read_csv(self.session_filename, sep="\t")
         self.stimuli_ls = self.stimuli_df["trial_type"]
-        print(self.stimuli_ls)
+        #print(self.stimuli_ls)
 
 
     def _run(self, run_number):
         """
         This function fetches a wave file and plays it.
-        It also provides some feedback to the user regarding:
-        - the number of the stimuli presentation (from 1 to 130)
-        - the inter-stimuli waiting periods.
+        It also provides some feedback to the user regarding
+        the number of the stimuli presentation (from 1 to 48)
         """
 
-        random_stimuli_ls = np.random.shuffle(self.stimuli_ls)
+        #random_stimuli_ls = np.random.shuffle(self.stimuli_ls)
+        #print(random_stimuli_ls)
         
-        for i, sound_file in enumerate(random_stimuli_ls):
+        for i, sound_file in enumerate(self.stimuli_ls):
             
+            print(sound_file)
             to_play = os.path.join(self.stimuli_path, sound_file)
+            print(to_play)
             
-            print(f"Presentation number {i}: {sound_file}")
+            print(f"Presentation number {i+1}: {sound_file}")
             
             # !!! Needs to be replaced by a psychopy functionality
             playsound(to_play)
