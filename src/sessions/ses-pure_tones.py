ad74f48efd4a426b8464269ef8f9850d02a743f0
efortier <eddy.fortier@umontreal.ca>
2022-01-03 17:13:24 -0500

    removed the list format in the tasks object declaration, added a loop to extract the subject number in the ID (ses-pure_tones.py)

--- src/sessions/ses-pure_tones.py
+++ src/sessions/ses-pure_tones.py
@@ -8,22 +8,27 @@ def get_tasks(parsed):
     from ..tasks.pure_tones import PureTones
     
     n_runs = 1
+    
+    sub_number = ""
+    for i in range(0, len(parsed.subject)):
+        if parsed.subject[i].isdigit():
+            sub_number += str(parsed.subject[i])
+        else:
+            continue
 
     session_design_filename = os.path.join(
         PURE_TONES_DATA_PATH,
         "auditory_designs",
-        f"sub-{parsed.subject}_ses-{parsed.session}_task-puretones_events.tsv",
+        f"sub-{sub_number}_ses-{parsed.session}_task-puretones_events.tsv",
     )
 
     n_runs_session = n_runs if int(parsed.session) > 1 else n_runs//2
 
-    tasks = [
-        PureTones(sub=parsed.subject,
-                  ISI=5,
-                  design=session_design_filename)
-        for run in range(1, n_runs_session + 1)
-    ]
-    #tasks.run(run_number=parsed.session)
+    tasks = PureTones(sub=parsed.subject,
+                      design=session_design_filename,
+                      ISI=5)
+
+    tasks._run(run_number=parsed.session)
     return tasks
 
 
