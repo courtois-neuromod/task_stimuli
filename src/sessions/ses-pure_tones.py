6b032a683be1ccd107b55c22cabe1590725dc6b2
basile <basile.pinsard@gmail.com>
2022-01-12 16:15:55 -0500

    use sound generated on the fly, fix task and sessions

--- src/sessions/ses-pure_tones.py
+++ src/sessions/ses-pure_tones.py
@@ -6,29 +6,22 @@ SOUND_PATH = os.path.join(PURE_TONES_DATA_PATH, "stimuli")
 
 def get_tasks(parsed):
     from ..tasks.pure_tones import PureTones
-    
+
     n_runs = 1
-    
-    sub_number = ""
-    for i in range(0, len(parsed.subject)):
-        if parsed.subject[i].isdigit():
-            sub_number += str(parsed.subject[i])
-        else:
-            continue
 
     session_design_filename = os.path.join(
         PURE_TONES_DATA_PATH,
         "auditory_designs",
-        f"sub-{sub_number}_ses-{parsed.session}_task-puretones_events.tsv",
+        f"sub-{parsed.subject}_ses-{parsed.session}_task-puretones_events.tsv",
     )
 
     n_runs_session = n_runs if int(parsed.session) > 1 else n_runs//2
 
-    tasks = PureTones(sub=parsed.subject,
+    tasks = [PureTones(sub=parsed.subject,
                       design=session_design_filename,
-                      ISI=5)
+                      ISI=5,
+                      name='task-puretones')]
 
-    tasks._run(run_number=parsed.session)
     return tasks
 
 
--- src/tasks/pure_tones.py
+++ src/tasks/pure_tones.py
@@ -5,12 +5,13 @@ import numpy as np
 # import psychtoolbox as ptb
 from psychopy import prefs
 from psychopy.sound.backend_ptb import SoundPTB as sound
+from psychopy.sound._base import apodize
 # from playsound import playsound
 
 from .task_base import Task
-# from ..shared import utils
+from ..shared import utils
 
-prefs.hardware['audioLib'] = ['PTB']
+#prefs.hardware['audioLib'] = ['PTB']
 
 # Time value when the experiment starts
 # t_initial = time.perf_counter()
@@ -18,8 +19,8 @@ prefs.hardware['audioLib'] = ['PTB']
 
 class PureTones(Task):
 
-    def __init__(self, sub, design, ISI=0):
-        super(Task, self).__init__()
+    def __init__(self, sub, design, ISI=0, **kwargs):
+        super().__init__(**kwargs)
         self.ISI = ISI
         self.sub = sub
         self.session_filename = design
@@ -35,33 +36,39 @@ class PureTones(Task):
         self.stimuli_path = os.path.join("data", "audio", "stimuli")
 
         self.stimuli_df = pd.read_csv(self.session_filename, sep="\t")
-        self.stimuli_ls = self.stimuli_df["trial_type"]
 
-    def _run(self, run_number):
+
+    def _run(self, exp_win, ctl_win):
         """
         This function fetches a wave file and plays it.
         It also provides some feedback to the user regarding
         the number of the stimuli presentation (from 1 to 48)
         """
 
-        for i, sound_file in enumerate(self.stimuli_ls):
+        for i, stimuli in self.stimuli_df.iterrows():
+
+            # reimplement
+            lr_idx = 0 if stimuli.ear == 'L' else 1
+            sampleRate = 22050
+            nSamples = int(stimuli.duration * sampleRate)
+            snd = np.zeros((nSamples,2))
+            snd[:,lr_idx] = np.sin(np.arange(0.0, 1.0, 1.0 / nSamples) * 2 * np.pi * stimuli.frequency * stimuli.duration)
+            snd[:,lr_idx] = apodize(snd[:,lr_idx], sampleRate)
 
             # to_play = os.path.join(self.stimuli_path, sound_file)
-            to_play = sound(value=os.path.join(self.stimuli_path, sound_file),
+            to_play = sound(value=snd,
                             stereo=True,
-                            sampleRate=44100,
-                            volume=0.5)
+                            secs=stimuli.duration,
+                            sampleRate=sampleRate,
+                            volume=np.power(10,stimuli.volume/20))
 
-            print(f"Presentation number {i+1}: {sound_file}")
+            print(f"Presentation number {i+1}: {stimuli.ear} {stimuli.frequency} {stimuli.volume} {np.power(10,stimuli.volume/20)}")
 
+            utils.wait_until(self.task_timer, stimuli.onset)
             # playsound(to_play)
             to_play.play()
-
-            print(f"Waiting for {self.ISI} second(s)")
-
-            # !!! Needs to be replaced by the utils wait function
-            time.sleep(self.ISI)
-            # utils.wait_until("self.task_timer", "ISI")
+            utils.wait_until(self.task_timer, stimuli.onset+stimuli.duration)
+            yield True
 
 
 # Calculation of the duration of the experiment
