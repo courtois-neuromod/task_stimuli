41b4ca9d34b5d401b53b2c15c9b461038afca596
basile <basile.pinsard@gmail.com>
2022-01-18 11:07:54 -0500

    triplet tasks:wip

--- src/sessions/ses-triplet_test.py
+++ src/sessions/ses-triplet_test.py
@@ -2,11 +2,7 @@ from ..tasks import language, task_base
 
 TASKS = [
     language.Triplet(
-        "data/language/triplet/first1000triples.csv", wait_key=True, name="triplet_test"
+        "data/language/triplet/test_triplets.tsv", name="task-triplets"
     ),
     task_base.Pause(text="Take a break. Press any key to continue...", wait_key=None),
-    language.Triplet(
-        "data/language/triplet/first1000triples.csv", wait_key=True, name="triplet_test"
-    ),
-    task_base.Pause(text="Take a break. Press <a> to continue...", wait_key=["a"]),
 ]
