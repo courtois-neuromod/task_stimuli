70f8e7eed9f116473248878ee1f24c750410ecb7
basile <basile.pinsard@gmail.com>
2022-01-18 11:07:54 -0500

    triplet tasks:wip

--- src/tasks/language.py
+++ src/tasks/language.py
@@ -2,29 +2,35 @@ import os, sys, time
 from psychopy import visual, core, data, logging, event
 from pandas import read_csv
 from .task_base import Task
+from colorama import Fore
 
 from ..shared import config, utils
 
-STIMULI_DURATION = 4
-BASELINE_BEGIN = 5
-BASELINE_END = 5
-TRIPLET_RIGHT_KEY = "l"
-TRIPLET_LEFT_KEY = "d"
-ISI = 2
+TR = 1.49
+STIMULI_DURATION = 2*TR
+BASELINE_BEGIN = 6
+BASELINE_END = 9
+TRIPLET_RIGHT_KEY = "r"
+TRIPLET_LEFT_KEY = "l"
+ISI = 1*TR
+
+INSTRUCTION_DURATION = 10
 
 
 class Triplet(Task):
 
-    DEFAULT_INSTRUCTION = """You will be presented three words.
+    DEFAULT_INSTRUCTION = """You will see three words: one at the top, two at the bottom.
 
-The one on top is the target.
-The two below are possible responses.
+We ask you to select which of the two words at the bottom is most related to the one at the top by pressing either the left or right button.
 
-You have to select the response (left or right) that is closest to the target."""
+Don't think too much and give the first answer that comes to mind
+"""
 
     INSTRUCTION_WAIT_KEY = (
         DEFAULT_INSTRUCTION + "\nWhen you're ready press <%s>" % TRIPLET_LEFT_KEY
     )
+
+    RESPONSE_KEYS= [TRIPLET_LEFT_KEY, TRIPLET_RIGHT_KEY]
 
     def __init__(self, words_file, *args, **kwargs):
         self.wait_key = kwargs.pop("wait_key", False)
@@ -46,79 +52,83 @@ You have to select the response (left or right) that is closest to the target.""
             wrapWidth=config.WRAP_WIDTH,
         )
 
-        def _draw_instr():
-            screen_text.draw(exp_win)
-            if ctl_win:
+        screen_text.draw(exp_win)
+        if ctl_win:
+            screen_text.draw(ctl_win)
+        yield True
+        time.sleep(INSTRUCTION_DURATION)
+        yield True
 
-                screen_text.draw(ctl_win)
-
-        if self.wait_key:
-            while True:
-                if len(event.getKeys([TRIPLET_LEFT_KEY])):
-                    break
-                _draw_instr()
-                yield
-        else:
-            for frameN in range(config.FRAME_RATE * config.INSTRUCTION_DURATION):
-                _draw_instr()
-                yield
-
-    def _run(self, exp_win, ctl_win):
-
-        self.trials = data.TrialHandler(self.words_list, 1, method="random")
-
-        target_stim = visual.TextStim(
+    def _setup(self, exp_win):
+        self.target_stim = visual.TextStim(
             exp_win, text="", pos=(0, 0.25), alignText="center", color="white"
         )
 
-        r1_stim = visual.TextStim(
+        self.r1_stim = visual.TextStim(
             exp_win, text="", pos=(-0.5, -0.25), alignText="center", color="white"
         )
 
-        r2_stim = visual.TextStim(
+        self.r2_stim = visual.TextStim(
             exp_win, text="", pos=(0.5, -0.25), alignText="center", color="white"
         )
 
+        self.trials = data.TrialHandler(self.words_list, 1, method="sequential")
+
+        self.duration = len(self.words_list)
+        self._progress_bar_refresh_rate = 2
+
+    def _run(self, exp_win, ctl_win):
+        yield True
         exp_win.logOnFlip(
             level=logging.EXP, msg="triplet: task starting at %f" % time.time()
         )
 
-        for frameN in range(config.FRAME_RATE * BASELINE_BEGIN):
-            yield ()
-        for trial_idx, trial in enumerate(self.trials):
-            target_stim.text = trial["target"]
-            r1_stim.text = trial["response1"]
-            r2_stim.text = trial["response2"]
+        for trial_n, trial in enumerate(self.trials):
+            self.target_stim.text = trial["target"]
+            self.r1_stim.text = trial["response1"]
+            self.r2_stim.text = trial["response2"]
 
-            exp_win.logOnFlip(level=logging.EXP, msg="triplet: %d" % trial_idx)
-            onset = self.task_timer.getTime()
-            # flush keys pressed before
-            event.getKeys([TRIPLET_LEFT_KEY, TRIPLET_RIGHT_KEY])
-            for frameN in range(config.FRAME_RATE * STIMULI_DURATION):
-                triplet_answer_keys = event.getKeys(
-                    [TRIPLET_LEFT_KEY, TRIPLET_RIGHT_KEY]
-                )
-                if len(triplet_answer_keys):
-                    self.trials.addData("answer", triplet_answer_keys[0])
-                    for frameNN in range(frameN, config.FRAME_RATE * STIMULI_DURATION):
-                        yield ()
-                    break
-                for stim in [target_stim, r1_stim, r2_stim]:
-                    stim.draw(exp_win)
-                    if ctl_win:
-                        stim.draw(ctl_win)
-                yield ()
+            responses = [trial["response1"], trial["response2"]]
+
+            exp_win.winHandle.activate()
+
+            for stim in [self.target_stim, self.r1_stim, self.r2_stim]:
+                stim.draw(exp_win)
+                if ctl_win:
+                    stim.draw(ctl_win)
+
+            exp_win.logOnFlip(level=logging.EXP, msg="triplet: %d" % trial_n)
+
+            # wait onset
+            utils.wait_until(self.task_timer, trial["onset"] - 1 / config.FRAME_RATE)
+            keypresses = event.getKeys(self.RESPONSE_KEYS) # flush response keys
+            yield True # flip
+            trial["onset_flip"] = (
+                self._exp_win_last_flip_time - self._exp_win_first_flip_time
+            )
+            self.progress_bar.set_description(
+                f"Trial {trial_n}:: "
+            )
+
+            utils.wait_until(self.task_timer, trial["onset"] + trial["duration"] - 1 / config.FRAME_RATE)
+            yield True
+            # record keypresses
+            triplet_answer_keys = event.getKeys(self.RESPONSE_KEYS, timeStamped=self.task_timer)
+            if len(triplet_answer_keys):
+                first_response = triplet_answer_keys[0]
+                response_idx = self.RESPONSE_KEYS.index(first_response[0])
+                self.trials.addData("answer", first_response[0])
+                self.trials.addData("answer_onset", first_response[1])
+                self.trials.addData("response_txt", responses[response_idx])
+                self.trials.addData("response_time", first_response[1]-trial["onset_flip"])
             else:
-                self.trials.addData("answer", "")  # no answer, too slow or asleep
-            offset = self.task_timer.getTime()
-            self.trials.addData("onset", onset)
-            self.trials.addData("offset", offset)
-            self.trials.addData("duration", offset - onset)  # RT or max stim duration
-            exp_win.logOnFlip(level=logging.EXP, msg="triplet: rest")
-            for frameN in range(config.FRAME_RATE * ISI):
-                yield ()
-        for frameN in range(config.FRAME_RATE * BASELINE_END):
-            yield ()
+                for k in ['answer', 'answer_onset', 'response_txt','response_time']:
+                    trial[k] = ''
+            self.trials.addData("all_keys", triplet_answer_keys)
+
+            trial["offset_flip"] = (
+                self._exp_win_last_flip_time - self._exp_win_first_flip_time
+            )
 
     def _save(self):
         self.trials.saveAsWideText(self._generate_unique_filename("events", "tsv"))
