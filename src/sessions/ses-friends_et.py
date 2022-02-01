bd5813b78bbcd0f9d4e32505d2e1b0a4f48a54c0
basile <basile.pinsard@gmail.com>
2022-02-01 14:15:35 -0500

    fix eyetracker, new camera

--- src/sessions/ses-friends_et.py
+++ src/sessions/ses-friends_et.py
@@ -8,5 +8,6 @@ for segment in "ab":
             "data/videos/friends/s2/friends_s2e%02d%s.mkv" % (episode, segment),
             aspect_ratio=4 / 3.0,
             name="task-friends-s2e%d%s" % (episode, segment),
+            use_eyetracking=True,
         )
     )
