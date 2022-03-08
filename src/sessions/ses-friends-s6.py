c99040f6e9153934d3f0635fc5d33ea47d32ec24
basile <basile.pinsard@gmail.com>
2022-03-08 09:38:05 -0500

    record eyetracking in friends s5/s6

--- src/sessions/ses-friends-s6.py
+++ src/sessions/ses-friends-s6.py
@@ -10,5 +10,6 @@ for episode in list(range(1, 16))+list(range(17,25)):
                 "data/videos/friends/s6/friends_s06e%02d%s.mkv" % (episode, segment),
                 aspect_ratio=4 / 3.0,
                 name="task-friends-s6e%d%s" % (episode, segment),
+                use_eyetracking=True,
             )
         )
