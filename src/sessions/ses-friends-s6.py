256cad1f93b1e6dda9eb61963fd893b123795bc6
basile <basile.pinsard@gmail.com>
2022-05-13 16:47:22 -0400

    add fixation dot at beginning and end of friends s6

--- src/sessions/ses-friends-s6.py
+++ src/sessions/ses-friends-s6.py
@@ -9,6 +9,7 @@ for episode in list(range(1, 16))+list(range(17,25)):
             video.SingleVideo(
                 "data/videos/friends/s6/friends_s06e%02d%s.mkv" % (episode, segment),
                 aspect_ratio=4 / 3.0,
+                fixations_duration=2.0,
                 name="task-friends-s6e%d%s" % (episode, segment),
             )
         )
