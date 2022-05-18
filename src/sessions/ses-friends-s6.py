b40b7ad5cdf291ca9718e917f62b47291bcceefe
MarieStLaurent <marie.stl@gmail.com>
2022-05-18 14:04:29 -0400

    added option of within-scan fixations

--- src/sessions/ses-friends-s6.py
+++ src/sessions/ses-friends-s6.py
@@ -4,9 +4,9 @@ def get_tasks(parsed):
     from ..tasks import video
     tasks = []
 
-    # TODO: de-comment line 9 when done piloting
-    for episode in list(range(1, 4)): # to test, just not to download the entire season for local tests
-    #for episode in list(range(1, 16))+list(range(17,25)):
+    # de-comment line 9 when done piloting
+    #for episode in list(range(1, 4)): # to test, just not to download the entire season for local tests
+    for episode in list(range(1, 16))+list(range(17,25)):
         segments = "abcd" if episode in (15,24) else "ab"
         for segment in segments:
             tasks.append(
