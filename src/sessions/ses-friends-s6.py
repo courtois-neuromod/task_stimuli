3758ffe7e176bff1980852ea102dbdb3d33ad2b9
MarieStLaurent <marie.stl@gmail.com>
2022-05-18 13:18:53 -0400

    added option of within-scan fixations

--- src/sessions/ses-friends-s6.py
+++ src/sessions/ses-friends-s6.py
@@ -1,15 +1,24 @@
-from ..tasks import video
 
-TASKS = []
+def get_tasks(parsed):
 
-for episode in list(range(1, 16))+list(range(17,25)):
-    segments = "abcd" if episode in (15,24) else "ab"
-    for segment in segments:
-        TASKS.append(
-            video.SingleVideo(
-                "data/videos/friends/s6/friends_s06e%02d%s.mkv" % (episode, segment),
-                aspect_ratio=4 / 3.0,
-                fixations_duration=2.0,
-                name="task-friends-s6e%d%s" % (episode, segment),
+    from ..tasks import video
+    tasks = []
+
+    # TODO: de-comment line 9 when done piloting
+    for episode in list(range(1, 4)): # to test, just not to download the entire season for local tests
+    #for episode in list(range(1, 16))+list(range(17,25)):
+        segments = "abcd" if episode in (15,24) else "ab"
+        for segment in segments:
+            tasks.append(
+                video.SingleVideo(
+                    "data/videos/friends/s6/friends_s06e%02d%s.mkv" % (episode, segment),
+                    aspect_ratio=4 / 3.0,
+                    endstart_fixduration=0.0,#2.0,
+                    inmovie_fixations=parsed.intask_fix,
+                    infix_freq=20,
+                    infix_dur=1.5,
+                    name="task-friends-s6e%d%s" % (episode, segment),
+                )
             )
-        )
+
+    return tasks
