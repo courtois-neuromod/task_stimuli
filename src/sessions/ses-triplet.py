f39386c9872bf5f331f3e2de54d0538070953534
basile <basile.pinsard@gmail.com>
2022-07-29 13:58:31 -0400

    final design with savestates

--- src/sessions/ses-triplet.py
+++ src/sessions/ses-triplet.py
@@ -16,49 +16,47 @@ def get_tasks(parsed):
     session = savestate['session']
     logging.exp(f"loading savestate: currently on session {savestate['session']:03d}")
 
-
     print(f'savestate: {savestate}')
-    #only one run of each
-    run = 1
 
-    word_fam_runs = [1, 2] if session == 1 else ([] if session==N_SESSIONS else [1])
-    triplet_runs =  [1, 2] if session == N_SESSIONS else ([] if session==1 else [1])
+    tasks_completed = True
 
-    for run in word_fam_runs:
-        task = language.WordFamiliarity(
-            f"{TRIPLET_DATA_PATH}/words_designs/sub-{parsed.subject}_ses-{savestate['session']:03d}_task-wordsfamiliarity_run-{run:02d}_design.tsv",
-            name=f"task-wordsfamiliarity_run-{run:02d}",
-            use_eyetracking=True,
-        )
+    for run in range(1, N_RUNS_PER_SESSION+1):
+        if session <= N_WORDS_SESSIONS:
+            task = language.WordFamiliarity(
+                f"{TRIPLET_DATA_PATH}/words_designs/sub-{parsed.subject}_ses-{session:03d}_task-wordsfamiliarity_run-{run:02d}_design.tsv",
+                name=f"task-wordsfamiliarity_run-{run:02d}",
+                use_eyetracking=True,
+            )
+        else:
+            task = language.Triplet(
+                f"{TRIPLET_DATA_PATH}/designs/sub-{parsed.subject}_ses-{session-N_WORDS_SESSIONS:03d}_task-triplet_run-{run:02d}_design.tsv",
+                name=f"task-triplets_run-{run:02d}",
+                use_eyetracking=True,
+            )
         yield task
-        tasks_completed = task._task_completed
+        tasks_completed = tasks_completed & task._task_completed
+
         yield task_base.Pause(
             text="You can take a short break.\n Press A when ready to continue",
             wait_key='a',
         )
-
-    for run in triplet_runs:
-        task = language.Triplet(
-            f"{TRIPLET_DATA_PATH}/designs/sub-{parsed.subject}_ses-{savestate['session']:03d}_task-triplet_run-{run:02d}_design.tsv",
-            name=f"task-triplets_run-{run:02d}",
-            use_eyetracking=True,
-        )
-        yield task
-        tasks_completed = tasks_completed & task._task_completed
 
     if tasks_completed:
         savestate['session'] += 1
         logging.exp(f"saving savestate: next session {savestate['session']:03d}")
         with open(savestate_path, 'w') as f:
             json.dump(savestate, f)
+    else:
+        print('ERROR: not all tasks were completed. This might be due to relaunching the task and skipping tasks.')
 
     return TASKS
 
 TRIPLET_DATA_PATH = "data/language/triplets"
 TR=1.49
-N_SESSIONS = 4
+N_SESSIONS = 6
+N_WORDS_SESSIONS = 5
 N_TRIALS_PER_RUN = 59 # 708/59=12
-N_RUNS_PER_SESSION = 3
+N_RUNS_PER_SESSION = 2
 STIMULI_DURATION = 4
 TRIAL_DURATION = 4*TR
 BASELINE_BEGIN = 6
