770f7c701bf0de89121535bd1008f31e50b6b578
test <basile.pinsard@gmail.com>
2022-08-03 11:44:35 -0400

    hotfixes

--- utils/check_mario_stable_progress.py
+++ utils/check_mario_stable_progress.py
@@ -1,7 +1,7 @@
 import glob, pandas
-for sub in [1,2,3, 6]:
+for sub in [1,2,3,5,6]:
     print(f"sub-{sub:02}" + '#'*50)
-    ev_fnames = glob.glob(f"sub-{sub:02d}/ses-*/*task-mario_*_events.tsv")
+    ev_fnames = glob.glob(f"sub-{sub:02d}/ses-*/*task-mario*_events.tsv")
     evs = [pandas.read_csv(ev_fname,delimiter='\t') for ev_fname in ev_fnames]
     evs_complete = [ev for ev in evs if 'questionnaire-answer' in ev.trial_type.values]
     evs_stable = [ev for ev  in evs_complete if len(ev.level.unique()) > 1 ]
@@ -11,4 +11,4 @@ for sub in [1,2,3, 6]:
     quest_onset = [ev[ev.trial_type=='questionnaire-value-change'].onset.values for ev in evs_stable]
     levels = pandas.concat([ev[ev.trial_type=='gym-retro_game'].level for ev in evs_stable])
     print("stable phase duration (min)", sum([qo[0] for qo in quest_onset if len(qo)])/60.)
-    #print(levels.value_counts().sort_index())
+    print(levels.value_counts().sort_index())
