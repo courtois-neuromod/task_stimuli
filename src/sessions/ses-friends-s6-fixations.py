2a6cfd139fa948eedc2830df059e06d4f8d3f7cb
MarieStLaurent <marie.stl@gmail.com>
2022-09-02 11:08:17 -0400

    debug session file

--- src/sessions/ses-friends-s6-fixations.py
+++ src/sessions/ses-friends-s6-fixations.py
@@ -2,6 +2,7 @@ from ..tasks import video
 
 TASKS = []
 
+
 # de-comment line 7 when done piloting
 #for episode in list(range(1, 4)): # to test, just not to download the entire season for local tests
 for episode in list(range(1, 16))+list(range(17,25)):
@@ -18,3 +19,31 @@ for episode in list(range(1, 16))+list(range(17,25)):
                 name="task-friends-s6e%d%s-fixations" % (episode, segment),
             )
         )
+'''
+
+TASKS.append(
+    video.SingleVideo(
+        "/home/labopb/Documents/Marie/neuromod/friends_eyetrack/video_stimuli/friends_s06e08a_extract2.mp4",
+        #"data/videos/friends/s6/friends_s06e%02d%s.mkv" % (episode, segment),
+        aspect_ratio=4 / 3.0,
+        startend_fixduration=0.0,#2.0,
+        inmovie_fixations=True,
+        infix_freq=20,
+        infix_dur=1.5,
+        name="task-friends-s6e08a2-fixations",
+    )
+)
+
+TASKS.append(
+    video.SingleVideo(
+        "/home/labopb/Documents/Marie/neuromod/friends_eyetrack/video_stimuli/friends_s06e08a_extract.mp4",
+        #"data/videos/friends/s6/friends_s06e%02d%s.mkv" % (episode, segment),
+        aspect_ratio=4 / 3.0,
+        startend_fixduration=0.0,#2.0,
+        inmovie_fixations=True,
+        infix_freq=20,
+        infix_dur=1.5,
+        name="task-friends-s6e08a1-fixations",
+    )
+)
+'''
