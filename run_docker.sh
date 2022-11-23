e677a16069bf4021b9c22711c792f9ecc030bb43
Hao-Ting Wang <htwangtw@gmail.com>
2022-11-23 13:14:30 -0500

    Move docker files to root for developing. Remember to revert when merging

--- run_docker.sh
+++ run_docker.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-docker run \
+sudo docker run \
   --network host \
   --device=/dev/dri \
   --device=/dev/snd  \
-  -v $PWD/src/tasks/videogame.py:/src/task_stimuli/src/tasks/videogame.py \
+  -v $PWD/src:/src/task_stimuli/src \
   -v /run/user/1000/pulse:/run/user/1000/pulse \
   -v $PWD/data:/src/task_stimuli/data\
-  -v ~/data/tests/test_docker:/data\
   -e HOST_UID=$(id -u)\
   -e HOST_GID=$(id -g) \
   -e DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -e PUPIL_PATH=/src/pupil \
   task_stimuli \
   python3 main.py $@
