555cd5d094081dd4259b5997b4e94f1eea668252
Hao-Ting Wang <htwangtw@gmail.com>
2023-01-26 15:46:24 -0500

    ADD test version of the rehersal and recall screen
    TO DOs:
    - handel ISI in display
    - add the rehersal time selection screen

--- run_docker.sh
+++ run_docker.sh
@@ -5,7 +5,7 @@ sudo docker run \
   --device=/dev/snd  \
   -v $PWD/src:/src/task_stimuli/src \
   -v /run/user/1000/pulse:/run/user/1000/pulse \
-  -v $PWD/data:/src/task_stimuli/data\
+  -v $PWD/data:/src/task_stimuli/data \
   -e HOST_UID=$(id -u)\
   -e HOST_GID=$(id -g) \
   -e DISPLAY \
