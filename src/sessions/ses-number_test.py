63cd0dc00522705e0a8e006f9485d3235327f352
Hao-Ting Wang <htwangtw@gmail.com>
2023-02-07 11:27:02 -0500

    TMP/FIX lock numpy version to prevent the numpy 1.24 error

--- src/sessions/ses-number_test.py
+++ src/sessions/ses-number_test.py
@@ -21,15 +21,16 @@ def get_tasks(parsed):
 
 
 # experiment
-
 allowed_aphabets = "ABCDEFGHJKLMNPQRSTUVWXYZ"  # remove O and I
-n_trials = 2
-n_pairs = 4
+n_blocks = 2
+n_pairs_range = (3, 8)  # this is called target score
 trial_duration = 10
 fixation_duration = 1
+performance_duration = 3
 
 def generate_memory_items(grid_size=(5, 4), n_pairs=4):
     # for now just generate 20 characters mixed with numbers and alphabets
+    # grid size is fixed
     import random
     items = [str(i + 1) for i in range(n_pairs)] * 2
     n_space = grid_size[0] * grid_size[1] - len(items)
@@ -45,32 +46,35 @@ def generate_isi(lb=0.50, ub=2.00):
 
 
 def generate_design_file(subject):
+    # sourcery skip: use-fstring-for-concatenation
     import pandas as pd
     import numpy as np
     import random
 
     trials = pd.DataFrame()
 
-    for i in range(n_trials):
+    for i in range(n_blocks):
         if i == 0:
             # initalise a block with question to select the rehersal time
             onset_trial = 0
-            grid = ["".join([" "] * 20)]
+            display = ["Please select the amount of time you wish to spend on rehersal."]
             onset = [onset_trial]
-            duration = [generate_isi() + fixation_duration]
+            duration = [generate_isi() + performance_duration]
             recall_display = ["N/A"]
             recall_ans = ["N/A"]
-            trial_type = ["fixation"]
-            trial_number = [0]
+            trial_type = ["estimate"]
+            trial_number = [i + 1]
             pair_number = [-1]
         else:
-            onset_trial = block['onset'].values[-1] + block['duration'].values[-1]
-            grid, onset, duration, recall_display, recall_ans, trial_type, trial_number, pair_number = [], [], [], [], [], [], [], []
+            onset_trial = block['onset'].values[-1] \
+                + block['duration'].values[-1]
+            (display, onset, duration, recall_display, recall_ans, trial_type,
+             trial_number, pair_number) = [], [], [], [], [], [], [], []
         # initialise a block
         block = pd.DataFrame()
         # rehersal
         memory_grid = generate_memory_items()
-        grid.append(memory_grid)  # generate rehersal trials
+        display.append(memory_grid)  # generate rehersal trials
         onset.append(onset_trial)
         duration.append(trial_duration)
         recall_display.append("N/A")
@@ -79,10 +83,11 @@ def generate_design_file(subject):
         trial_number.append(i + 1)
         pair_number.append(0)
 
+        n_pairs = random.randrange(n_pairs_range[0], n_pairs_range[-1] + 1)
         # recall
         for j in range(n_pairs):
             # fixation
-            grid.append("".join([" "] * 20))
+            display.append("".join([" "] * 20))
             onset.append(onset[-1] + duration[-1])
             duration.append((generate_isi() + fixation_duration))
             recall_display.append("N/A")
@@ -99,12 +104,18 @@ def generate_design_file(subject):
             mem_grid = memory_grid
             # find the location index of the current number
             mem_grid = re.sub(pattern, r' ', mem_grid)
-            current_number_loc = [loc for loc, c in enumerate(mem_grid) if c.isdigit()]
-            random.shuffle(current_number_loc)  # shuffle
-            current_ans_loc = current_number_loc[0] # the first one will be the answer
-            current_display_loc = current_number_loc[-1] # the last one will be the displayed
-            mem_grid = f"{mem_grid[:current_ans_loc]} {mem_grid[current_ans_loc + 1:]}"  # remove the answer
-            grid.append(mem_grid)
+            current_number_loc = [loc
+                                  for loc, c in enumerate(mem_grid)
+                                  if c.isdigit()]
+            random.shuffle(current_number_loc)
+            # the first one will be the answer
+            current_ans_loc = current_number_loc[0]
+            # the last one will be the displayed
+            current_display_loc = current_number_loc[-1]
+            # remove the answer from the grid
+            mem_grid = mem_grid[:current_ans_loc] + " " \
+                + mem_grid[current_ans_loc + 1:]
+            display.append(mem_grid)
             onset.append(onset[-1] + duration[-1] )
             duration.append(trial_duration)
             recall_display.append(current_display_loc)
@@ -113,8 +124,28 @@ def generate_design_file(subject):
             trial_number.append(i + 1)
             pair_number.append(j + 1)
 
-        # fixation
-        grid.append("".join([" "] * 20))
+        # ending recall trials with a fixation
+        display.append("".join([" "] * 20))
+        onset.append(onset[-1] + duration[-1])
+        duration.append((generate_isi() + fixation_duration))
+        recall_display.append("N/A")
+        recall_ans.append("N/A")
+        trial_type.append("fixation")
+        trial_number.append(0)
+        pair_number.append(-1)
+
+        # how many pairs did you get right?
+        display.append("How many pairs did you get right?")
+        onset.append(onset[-1] + duration[-1])
+        duration.append((generate_isi() + performance_duration))
+        recall_display.append("N/A")
+        recall_ans.append("N/A")
+        trial_type.append("performance")
+        trial_number.append(i + 1)
+        pair_number.append(-1)
+
+        # ending block with a fixation
+        display.append("".join([" "] * 20))
         onset.append(onset[-1] + duration[-1])
         duration.append((generate_isi() + fixation_duration))
         recall_display.append("N/A")
@@ -125,7 +156,7 @@ def generate_design_file(subject):
 
         block['trial_number'] = trial_number
         block['pair_number'] = pair_number
-        block['grid'] = grid
+        block['display'] = display
         block['duration'] = duration
         block['onset'] = onset
         block['recall_display'] = recall_display
@@ -133,15 +164,13 @@ def generate_design_file(subject):
         block['trial_type'] =trial_type
 
         trials = pd.concat((trials, block))
-    # generate recall trials
-    trials = trials.reset_index(drop=True)
 
     out_fname = os.path.join(
         NUMBER_PAIRS_DATA_PATH,
         "designs",
         f"sub-{subject}_design.tsv",
     )
-    trials.to_csv(out_fname, sep="\t", index=True, index_label="index")
+    trials.to_csv(out_fname, sep="\t", index=True)
 
 
 if __name__ == "__main__":
