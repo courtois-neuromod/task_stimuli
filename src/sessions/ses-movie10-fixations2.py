0a6bf36d5411241b4601c67277e124098d169c38
MarieStLaurent <marie.stl@gmail.com>
2023-02-24 15:02:04 -0500

    updated session files movie10+fix

--- src/sessions/ses-movie10-fixations2.py
+++ src/sessions/ses-movie10-fixations2.py
@@ -5,51 +5,51 @@ TASKS = []
 seg_idx = 7
 TASKS.append(
     video.SingleVideo(
-        "data/videos/bourne_supremacy/bourne_supremacy_seg%02d.mkv" % seg_idx,
+        "data/videos/bourne/bourne%02d.mkv" % seg_idx,
         aspect_ratio=372 / 157,
         startend_fixduration=0.0,#2.0,
         inmovie_fixations=True,
         infix_freq=20,
         infix_dur=1.5,
-        name="bourne_supremacy_seg-%d-fixations" % seg_idx,
+        name="bourne_seg-%d-fixations" % seg_idx,
     )
 )
 
-seg_idx = 5
+seg_idx = 4
 TASKS.append(
     video.SingleVideo(
-        "data/videos/life1/life1_seg%02d.mkv" % seg_idx,
+        "data/videos/life/life%02d.mkv" % seg_idx,
         aspect_ratio=12 / 5,
         startend_fixduration=0.0,#2.0,
         inmovie_fixations=True,
         infix_freq=20,
         infix_dur=1.5,
-        name="life1_seg-%d-fixations" % seg_idx,
+        name="life_seg-%d-fixations" % seg_idx,
     )
 )
 
 seg_idx = 8
 TASKS.append(
     video.SingleVideo(
-        "data/videos/hidden_figures/hidden_figures_seg%02d.mkv" % seg_idx,
+        "data/videos/figures/figures%02d.mkv" % seg_idx,
         aspect_ratio=12 / 5,
         startend_fixduration=0.0,#2.0,
         inmovie_fixations=True,
         infix_freq=20,
         infix_dur=1.5,
-        name="hidden_figures_seg-%d-fixations" % seg_idx,
+        name="figures_seg-%d-fixations" % seg_idx,
     )
 )
 
 seg_idx = 11
 TASKS.append(
     video.SingleVideo(
-        "data/videos/the_wolf_of_wall_street/the_wolf_of_wall_street_seg%02d.mkv" % seg_idx,
+        "data/videos/wolf/wolf%02d.mkv" % seg_idx,
         aspect_ratio=12 / 5,
         startend_fixduration=0.0,#2.0,
         inmovie_fixations=True,
         infix_freq=20,
         infix_dur=1.5,
-        name="the_wolf_of_wall_street_seg-%d-fixations" % seg_idx,
+        name="wolf_seg-%d-fixations" % seg_idx,
     )
 )
