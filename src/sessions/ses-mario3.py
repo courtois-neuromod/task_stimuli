e0c0c526043b8f65447af8238cb2666cea72d020
basile <basile.pinsard@gmail.com>
2023-03-15 09:51:58 -0400

    revert

--- src/sessions/ses-mario3.py
+++ src/sessions/ses-mario3.py
@@ -109,7 +109,7 @@ def get_tasks(parsed):
             state_names=next_levels,
             scenarii=[scenario]*len(next_levels),
             repeat_scenario=True,
-            max_duration=30,  # if when level completed or dead we exceed that time in secs, stop the task
+            max_duration=7 * 60,  # if when level completed or dead we exceed that time in secs, stop the task
             name=f"task-mario3_run-{run+1:02d}",
             instruction="playing Super Mario 3 \n\n Please fixate the dot in between repetitions!",
             post_run_ratings = [(k, q, 7) for k, q in enumerate(other_ratings+flow_ratings)],
