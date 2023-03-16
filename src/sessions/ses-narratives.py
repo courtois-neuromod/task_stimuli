427116b57a7658747c625a215bf4c678c6787b38
basile <basile.pinsard@gmail.com>
2023-03-16 16:40:54 -0400

    wip

--- src/sessions/ses-narratives.py
+++ src/sessions/ses-narratives.py
@@ -1,8 +1,10 @@
 from ..tasks.narratives import Story, FreeRecall
 
 TASKS = [
-    FreeRecall(device='', name="task-lucyrecall_run-01"),
+
+    FreeRecall(name="task-lucyrecall_run-01", max_duration=30),
     Story(
         sound_file='./data/narratives.stimuli/audio_files/test.wav',
         name="task-lucy_run-01"),
+
 ]
