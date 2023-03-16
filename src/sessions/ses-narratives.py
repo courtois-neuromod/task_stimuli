bcd0b2ec3d02878c81a6be94fd378ab4d81eaa61
basile <basile.pinsard@gmail.com>
2023-03-16 09:15:21 -0400

    narratives: FreeRecall , wip

--- src/sessions/ses-narratives.py
+++ src/sessions/ses-narratives.py
@@ -1,7 +1,8 @@
-from ..tasks.narratives import Story
+from ..tasks.narratives import Story, FreeRecall
 
 TASKS = [
+    FreeRecall(device='', name="task-lucyrecall_run-01"),
     Story(
         sound_file='./data/narratives.stimuli/audio_files/test.wav',
-        name="task-lucy_run-01")
+        name="task-lucy_run-01"),
 ]
--- src/tasks/narratives.py
+++ src/tasks/narratives.py
@@ -64,5 +64,74 @@ class Story(SoundTaskBase):
     """docstring for Story."""
 
     DEFAULT_INSTRUCTION = """
-    Listen to the story.
+    Please listen to the following story carefully.
+    You will be asked a few questions once it is over..
 """
+
+
+class FreeRecall(Task):
+    """docstring for FreeRecall."""
+
+    DEFAULT_INSTRUCTION = """
+    Please provide an account of what you heard.
+    Remember you have as much time as you need.
+    Please provide as many details as you can.
+    Please try to keep your head as still as possible while you talk.
+    Please start talking when instructed on the screen.
+    Press A when done.
+"""
+
+    def __init__(self, mic_device, initial_wait=4, final_wait=9, max_duration=600, *args, **kwargs):
+        super().__init__(**kwargs)
+        self.initial_wait, self.final_wait, self.max_duration = initial_wait, final_wait, max_duration
+        self.mic_device = mic_device
+
+
+    def _setup(self, exp_win,):
+        import psychopy.sound
+        self._mic = psychopy.sound.Microphone(
+            device = mic_device
+        )
+        self.screen_text = visual.TextStim(
+            exp_win,
+            text=self.instruction,
+            alignText="center",
+            color="white",
+            wrapWidth=config.WRAP_WIDTH,
+        )
+
+
+    def _run(self, exp_win, ctl_win):
+        self._mic.start()
+
+        for _ in utils.wait_until_yield(
+            self.task_timer,
+            self.initial_wait,
+            keyboard_accuracy=.1):
+            self._mic.poll()
+            yield False
+
+        self.screen_text.draw(exp_win)
+        if ctl_win:
+            self.screen_text.draw(ctl_win)
+
+        for _ in utils.wait_until_yield(
+            self.task_timer,
+            self.max_duration - self.final_wait,
+            keyboard_accuracy=.1):
+            self._mic.poll()
+            yield False
+
+        yield True
+        for _ in utils.wait_until_yield(
+            self.task_timer,
+            self.task_timer.getTime() + self.final_wait,
+            keyboard_accuracy=.1):
+            self._mic.poll()
+            yield False
+        self._mic.stop()
+        self._audioClip = mic.getRecording()
+
+    def _save(self):
+        output_wav_file = self._generate_unique_filename("audio", "wav")
+        self._audioClip.save(output_wav_file)
