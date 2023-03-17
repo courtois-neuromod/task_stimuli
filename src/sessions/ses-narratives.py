62fd3f0347b82ce9c1b62515f199bc5106490f5c
basile <basile.pinsard@gmail.com>
2023-03-17 15:29:05 -0400

    narratives, wip,

--- src/sessions/ses-narratives.py
+++ src/sessions/ses-narratives.py
@@ -1,14 +1,20 @@
-
+import os
 
 from ..tasks.narratives import Story, FreeRecall, RecencyJudgments
 
-TASKS = [
-    RecencyJudgments(
-        design_file='data/narratives.stimuli/recency_segments/lucy/first_viewing_lucy.csv',
-        name="task-lucyrecency_run-01"),
-    FreeRecall(name="task-lucyrecall_run-01", max_duration=30),
-    Story(
-        sound_file='./data/narratives.stimuli/audio_files/test.wav',
-        name="task-lucy_run-01"),
+STORIES = ['21styear','black','forgot','lucy','notthefallintact','pieman','prettymouth','slumlordreach','tunnel']
+STORIES_DURATIONS = [3374,800,837,542,581,450,712,1801,1533]
 
-]
+STIMULI_PATH  = 'data/narratives.stimuli'
+
+def get_tasks(parsed):
+
+    for story, story_duration in zip(STORIES, STORIES_DURATIONS):
+        story_cap = story.capitalize()
+        yield Story(
+            sound_file=os.path.join(STIMULI_PATH, 'audio_files', f"{story}_audio.wav"),
+            name=f"task-{story_cap}Story_run-01")
+        yield FreeRecall(name="task-{story_cap}Recall_run-01", max_duration=story_duration*.4)
+        yield RecencyJudgments(
+                design_file=os.path.join(STIMULI_PATH, 'recency_segments', story, f"first_viewing_{story}.csv"),
+                name="task-{story_cap}Recency_run-01"),
