6419494d8f7701f7ce3624f2a4b0549414e616df
basile <basile.pinsard@gmail.com>
2023-03-17 11:47:00 -0400

    RecencyJudgments almost there

--- src/sessions/ses-narratives.py
+++ src/sessions/ses-narratives.py
@@ -1,7 +1,11 @@
-from ..tasks.narratives import Story, FreeRecall
+
+
+from ..tasks.narratives import Story, FreeRecall, RecencyJudgments
 
 TASKS = [
-
+    RecencyJudgments(
+        design_file='data/narratives.stimuli/recency_segments/lucy/first_viewing_lucy.csv',
+        name="task-lucyrecency_run-01"),
     FreeRecall(name="task-lucyrecall_run-01", max_duration=30),
     Story(
         sound_file='./data/narratives.stimuli/audio_files/test.wav',
