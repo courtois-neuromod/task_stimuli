d916bec8c952a5fe8f66109dcb6e21c6d55796dc
basile <basile>
2023-09-01 15:45:37 -0400

    add friends s7

---
+++ src/sessions/ses-friends-s7.py
@@ -0,0 +1,16 @@
+from ..tasks import video
+
+TASKS = []
+
+for episode in list(range(1,24)):
+    segments = "abcd" if episode in (23,) else ("abc" if episode in (16) else "ab")
+    for segment in segments:
+        TASKS.append(
+            video.SingleVideo(
+                "data/videos/friends/s7/friends_s07e%02d%s.mkv" % (episode, segment),
+                aspect_ratio=4 / 3.0,
+                startend_fixduration=2.0,
+                name="task-friends-s7e%d%s" % (episode, segment),
+                use_eyetracking=True,
+            )
+        )
