db520cc6e784eec39056ef6c370b19c9c5a79549
basile <basile>
2023-11-29 12:49:58 -0500

    activate ET

--- src/sessions/ses-mutemusic.py
+++ src/sessions/ses-mutemusic.py
@@ -10,4 +10,8 @@ def get_tasks(parsed):
     for pli in range(last_playlist, 7):
         playlist_file = f'Sub-{parsed.subject}_Playlist_{pli}.tsv'
         playlist_path = os.path.join(STIMULI_PATH, f'Sub-{parsed.subject}', playlist_file)
-        yield Playlist(tsv_path=playlist_path, name=f"task-mutemusic_run-{pli}")
+        yield Playlist(
+            tsv_path=playlist_path,
+            use_eyetracking=True,
+            et_calibrate=pli==last_playlist,
+            name=f"task-mutemusic_run-{pli}")
