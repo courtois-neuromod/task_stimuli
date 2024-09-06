b1e2e0065db739cc1ebfaa28a08e2b9f077aa127
Yann Harel <yharel109@gmail.com>
2024-09-05 23:41:24 -0400

    integrated eeg triggers to task_base

--- src/shared/eeg.py
+++ src/shared/eeg.py
@@ -14,23 +14,24 @@ EEG_settings = {
 
 port = None
 current_signal = 0
+reset = 0
 
 def send_signal(data):
     global port
     if not port:
-        port = serial.Serial(SERIAL_PORT_ADDRESS)
+        port = serial.Serial(config.SERIAL_PORT_ADDRESS)
     start=time.monotonic()
     port.write(data.to_bytes(1, byteorder='big'))
     # hog cpu for accurate timing
     while time.monotonic() < start + EEG_MARKER_DURATION:
         continue
-    port.write(0.to_bytes(1, byteorder='big'))  # reset
+    port.write(reset.to_bytes(1, byteorder='big'))  # reset
 
 
 def set_trigger_signal():
     global port, current_signal
     if not port:
-    	port = serial.Serial(SERIAL_PORT_ADDRESS)
+        port = serial.Serial(config.SERIAL_PORT_ADDRESS)
     new_signal = 0 if current_signal else EEG_settings["TASK_FLIP"]
     port.write(new_signal.to_bytes(1, byteorder='big'))
     current_signal = new_signal
