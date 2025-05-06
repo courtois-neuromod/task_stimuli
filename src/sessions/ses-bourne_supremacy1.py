ef96f088452ab8447a10d47fdc6ef478912367b6
VPixx <vpixx@vpixx-linux-machine.vpixx.com>
2025-05-06 12:33:23 -0400

    updated path to bourne videos

--- src/sessions/ses-bourne_supremacy1.py
+++ src/sessions/ses-bourne_supremacy1.py
@@ -4,7 +4,7 @@ TASKS = []
 for seg_idx in range(1, 6):
     TASKS.append(
         video.SingleVideo(
-            "data/videos/bourne_supremacy/bourne_supremacy_seg%02d.mkv" % seg_idx,
+            "data/videos/movie10/bourne/bourne%02d.mkv" % seg_idx,
             aspect_ratio=372 / 157,
             name="bourne_supremacy_seg-%d" % seg_idx,
         )
